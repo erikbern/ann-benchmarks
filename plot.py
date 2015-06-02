846c2362e2846115145417d95373b2bec54510fb
Erik Bernhardsson <mail@erikbern.com>
2015-06-02 15:14:33 -0400

    new data w kgraph

--- plot.py
+++ plot.py
@@ -7,12 +7,16 @@ for line in open('data.tsv'):
     algo, algo_name, build_time, search_time, precision = line.strip().split('\t')
     all_data.setdefault(algo, []).append((algo_name, float(build_time), float(search_time), float(precision)))
 
-for algo, data in all_data.iteritems():
+colors = plt.cm.rainbow(numpy.linspace(0, 1, len(all_data)))
+handles = []
+
+for algo, color in zip(all_data.keys(), colors):
+    data = all_data[algo]
     data.sort(key=lambda t: t[-2]) # sort by time
     xs = [t[-2] for t in data]
     ys = [t[-1] for t in data]
     ls = [t[0] for t in data]
-    # plt.plot(xs, ys, 'x', label=algo)
+    plt.plot(xs, ys, 'o', label=algo, color=color)
     #for i, l in enumerate(ls):
     #    plt.annotate(ls[i], (xs[i], ys[i]), color='lightgray', fontsize=8)
 
@@ -25,13 +29,14 @@ for algo, data in all_data.iteritems():
             last_y = y
             ys.append(t[-1])
             xs.append(t[-2])
-    plt.plot(xs, ys, 'x-', label=algo)
+    handle, = plt.plot(xs, ys, 'o-', label=algo, color=color)
+    handles.append(handle)
 
 plt.gca().set_xscale('log')
 plt.gca().set_title('Precision-Performance tradeoff - up and to the left is better')
 plt.gca().set_xlabel('Time per query (s) - lower is better')
 plt.gca().set_ylabel('10-NN precision - higher is better')
-plt.gca().legend()
+plt.gca().legend(handles=handles)
 plt.grid(b=True, which='both', color='0.65',linestyle='-')
 
 plt.savefig('plot.png')
