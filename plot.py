17f1be80f86b4af4320a1bb1eef057b0bb8fe898
Erik Bernhardsson <mail@erikbern.com>
2015-06-02 15:05:12 -0400

    plot pareto frontier

--- plot.py
+++ plot.py
@@ -12,9 +12,20 @@ for algo, data in all_data.iteritems():
     xs = [t[-2] for t in data]
     ys = [t[-1] for t in data]
     ls = [t[0] for t in data]
-    plt.plot(xs, ys, 'x-', label=algo)
+    # plt.plot(xs, ys, 'x', label=algo)
     #for i, l in enumerate(ls):
     #    plt.annotate(ls[i], (xs[i], ys[i]), color='lightgray', fontsize=8)
+
+    # Plot Pareto frontier
+    xs, ys = [], []
+    last_y = float('-inf')
+    for t in data:
+        y = t[-1]
+        if y > last_y:
+            last_y = y
+            ys.append(t[-1])
+            xs.append(t[-2])
+    plt.plot(xs, ys, 'x-', label=algo)
 
 plt.gca().set_xscale('log')
 plt.gca().set_title('Precision-Performance tradeoff - up and to the left is better')
