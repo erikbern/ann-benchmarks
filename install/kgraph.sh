94a459e1f825e7e2748f475f7f2c0e4e0b27af94
Erik Bernhardsson <mail@erikbern.com>
2015-06-02 10:42:40 +0000

    kgraph install script working

--- install/kgraph.sh
+++ install/kgraph.sh
@@ -1,5 +1,11 @@
 git clone https://github.com/aaalgo/kgraph
-sudo apt-get install -y libboost-timer-dev libboost-chrono-dev libboost-program-options-dev libboost-system-dev
+sudo apt-get install -y libboost-timer-dev libboost-chrono-dev libboost-program-options-dev libboost-system-dev libboost-python-dev
 cd kgraph
 make
+make release
+cd kgraph-release/python
+make
+cd ..
+sudo cp python/pykgraph.so /usr/local/lib/python2.7/dist-packages
+sudo cp bin/libkgraph.so /usr/lib
 
