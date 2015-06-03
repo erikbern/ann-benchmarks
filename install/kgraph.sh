c1d6cafc4bbe476a994a81fd329d2bc8b47e542c
Ubuntu <ubuntu@ip-172-31-37-149.us-west-2.compute.internal>
2015-06-03 23:22:09 +0000

    simplified kgraph installation

--- install/kgraph.sh
+++ install/kgraph.sh
@@ -1,11 +1,8 @@
 git clone https://github.com/aaalgo/kgraph
-sudo apt-get install -y libboost-timer-dev libboost-chrono-dev libboost-program-options-dev libboost-system-dev libboost-python-dev
-cd kgraph
+pushd kgraph
+sudo make deps-ubuntu
 make
 make release
-cd kgraph-release/python
-make
-cd ..
 sudo cp python/pykgraph.so /usr/local/lib/python2.7/dist-packages
-sudo cp bin/libkgraph.so /usr/lib
-cd ../..
+sudo cp kgraph-release/bin/libkgraph.so /usr/lib
+cd ..
