9cb85ea5a9df7ea6aca85ff2695c74e61fbfcb04
Ubuntu <ubuntu@ip-172-31-13-199.us-west-2.compute.internal>
2015-06-07 23:10:31 +0000

    delete temp files

--- install/sift.sh
+++ install/sift.sh
@@ -1,5 +1,7 @@
 wget "ftp://ftp.irisa.fr/local/texmex/corpus/sift.tar.gz"
 tar -xzf sift.tar.gz
+rm -rf sift.tar.gz
 wget "https://raw.githubusercontent.com/searchivarius/NonMetricSpaceLib/master/data/data_conv/convert_texmex_fvec.py"
 python convert_texmex_fvec.py sift/sift_base.fvecs >> sift.txt
+rm -rf sift
 
