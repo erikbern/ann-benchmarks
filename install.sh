9cdb9c108dc41d9145b282582ca47ed2fd2ee47b
searchivarius <leo@boytsov.info>
2015-06-12 04:28:26 -0400

    Adding nmslib.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -y python-numpy python-scipy
 cd install
-for fn in annoy.sh panns.sh nearpy.sh sklearn.sh flann.sh kgraph.sh glove.sh sift.sh
+for fn in annoy.sh panns.sh nearpy.sh sklearn.sh flann.sh kgraph.sh nmslib.sh glove.sh sift.sh
 do
     source $fn
 done
