0e14d70eb6600cb7afcc90c4798af7c4ad93f2a6
Ubuntu <ubuntu@ip-172-31-6-1.us-west-2.compute.internal>
2015-06-14 19:43:16 +0000

    fix installation

--- install/annoy.sh
+++ install/annoy.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y python-dev
+sudo apt-get install -y python-dev python-setuptools
 git clone https://github.com/spotify/annoy
 cd annoy
 sudo python setup.py install
--- install/nearpy.sh
+++ install/nearpy.sh
@@ -1,1 +1,2 @@
+sudo apt-get install -y python-pip
 sudo pip install nearpy bitarray redis
