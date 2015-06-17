9aa12719d48966745f8cafcc05d528e09bbbaea3
Erik Bernhardsson <erikbern@spotify.com>
2015-06-18 00:03:12 +0200

    trying to add tox & setting up travis

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y python-numpy python-scipy python-sklearn
+apt-get install -y python-numpy python-scipy python-sklearn
 cd install
 for fn in annoy.sh panns.sh nearpy.sh sklearn.sh flann.sh kgraph.sh nmslib.sh glove.sh sift.sh
 do
