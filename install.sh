74c397d61ef4ab054094ed09927a1831df285d96
Erik Bernhardsson <erikbern@spotify.com>
2015-06-18 18:54:41 +0200

    don't download big datasets by default

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt-get install -y python-numpy python-scipy python-sklearn
 cd install
-for fn in annoy.sh panns.sh nearpy.sh sklearn.sh flann.sh kgraph.sh nmslib.sh glove.sh sift.sh
+for fn in annoy.sh panns.sh nearpy.sh sklearn.sh flann.sh kgraph.sh nmslib.sh
 do
     source $fn
 done
