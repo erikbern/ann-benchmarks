497102deea240d802d411b76d2b637bd38a5f47f
Erik Bernhardsson <mail@erikbern.com>
2015-06-21 16:16:36 +0000

    nearpy fix

--- install/nearpy.sh
+++ install/nearpy.sh
@@ -1,3 +1,3 @@
-apt-get install -y python-pip libhdf5-dev
+apt-get install -y python-pip libhdf5-openmpi-dev python-h5py
 pip install cython
-pip install nearpy bitarray redis h5py
+pip install nearpy bitarray redis
