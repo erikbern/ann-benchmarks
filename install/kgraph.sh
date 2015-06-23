700305d3938e484df20916cdc5921db57646f755
Erik Bernhardsson <erikbern@spotify.com>
2015-06-23 12:16:38 +0200

    use official kgraph version + some more results

--- install/kgraph.sh
+++ install/kgraph.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/erikbern/kgraph
+git clone https://github.com/aaalgo/kgraph
 apt-get install -y libboost-timer1.48-dev libboost-chrono1.48-dev libboost-program-options1.48-dev libboost-system1.48-dev
 apt-get autoremove -y
 
