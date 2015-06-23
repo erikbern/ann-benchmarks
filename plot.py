bcfe3747a97643adea3e049842759bb6a4b2fed1
Erik Bernhardsson <erikbern@spotify.com>
2015-06-23 13:47:19 +0200

    plot y axis as 'queries per second'
    (instead of 'time per query' with inverted scale - marginally more logical)

--- plot.py
+++ plot.py
@@ -23,8 +23,8 @@ labels = []
 for algo, color in zip(all_data.keys(), colors):
     data = all_data[algo]
     data.sort(key=lambda t: t[-2]) # sort by time
-    xs = [t[-2] for t in data]
-    ys = [t[-1] for t in data]
+    ys = [1.0 / t[-2] for t in data] # queries per second
+    xs = [t[-1] for t in data]
     ls = [t[0] for t in data]
     plt.plot(xs, ys, 'o', label=algo, color=color, markersize=3)
     #for i, l in enumerate(ls):
@@ -37,20 +37,20 @@ for algo, color in zip(all_data.keys(), colors):
         y = t[-1]
         if y > last_y:
             last_y = y
-            ys.append(t[-1])
-            xs.append(t[-2])
+            xs.append(t[-1])
+            ys.append(1.0 / t[-2])
     handle, = plt.plot(xs, ys, 'o-', label=algo, color=color)
     handles.append(handle)
     labels.append(algo)
 
-plt.gca().set_xscale('log')
-plt.gca().set_title('Precision-Performance tradeoff - up and to the left is better')
-plt.gca().set_xlabel('Time per query (s) - lower is better')
-plt.gca().set_ylabel('10-NN precision - higher is better')
+plt.gca().set_yscale('log')
+plt.gca().set_title('Precision-Performance tradeoff - up and to the right is better')
+plt.gca().set_ylabel('Queries per second ($s^{-1}$) - larger is better')
+plt.gca().set_xlabel('10-NN precision - larger is better')
 box = plt.gca().get_position()
 plt.gca().set_position([box.x0, box.y0, box.width * 0.8, box.height])
 plt.gca().legend(handles, labels, loc='center left', bbox_to_anchor=(1, 0.5))
 plt.grid(b=True, which='both', color='0.65',linestyle='-')
-plt.ylim([0, 1.03])
+plt.xlim([0.0, 1.03])
 plt.savefig(args.output, bbox_inches='tight')
 
