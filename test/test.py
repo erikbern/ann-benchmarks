2fc9ba306076e829a9612e9ce60bbe5294759740
Erik Bernhardsson <erikbern@spotify.com>
2015-07-19 15:37:41 -0400

    run tests in subprocess in case they segfault

--- test/test.py
+++ test/test.py
@@ -3,15 +3,27 @@ import inspect
 import ann_benchmarks
 from sklearn.datasets.samples_generator import make_blobs
 import warnings
+import multiprocessing
 
 # Generate dataset
 X, labels_true = make_blobs(n_samples=10000, n_features=10,
                             centers=10, cluster_std=5,
                             random_state=0)
 
-def check_algo(algo_name, algo):
+def fit_and_query(algo, result_queue):
+    # Run algorithm in subprocess in case it segfaults
     algo.fit(X)
     result = algo.query(X[42], 10)
+    result_queue.put(result)
+
+
+def check_algo(algo_name, algo):
+    queue = multiprocessing.Queue()
+    p = multiprocessing.Process(target=fit_and_query, args=(algo, queue))
+    p.start()
+    p.join()
+    result = queue.get(timeout=10.0)
+
     if len(result) != 10:
         warnings.warn('%s: Expected results to have length 10: Result: %s' % (algo_name, result))
     if len(set(result)) != 10:
