6ca29807fc36b2b2e39e3c256d880b8309773b6e
Erik Bernhardsson <mail@erikbern.com>
2015-07-21 02:51:04 +0000

    fixed flann version crap

--- install/flann.sh
+++ install/flann.sh
@@ -2,7 +2,6 @@ cd "$(dirname "$0")"
 git clone https://github.com/mariusmuja/flann
 sudo apt-get install -y cmake
 cd flann
-git checkout bd1410dff94ee001b04de4a26a8f3ecefa155cb0 # Last version that builds on Ubuntu 12.04
 mkdir build
 cd build
 cmake ..
