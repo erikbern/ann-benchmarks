b8b0e6fce119a52363f059f0da2820e0b5fefa84
Ilya Razenshteyn <ilyaraz@mit.edu>
2015-11-30 12:25:43 -0800

    adjust glove.sh to the new URLs for the GloVe datasets

--- install/glove.sh
+++ install/glove.sh
@@ -1,5 +1,5 @@
 cd "$(dirname "$0")"
-wget "http://www-nlp.stanford.edu/data/glove.twitter.27B.100d.txt.gz"
-gunzip -d glove.twitter.27B.100d.txt.gz
+wget "http://nlp.stanford.edu/data/glove.twitter.27B.zip"
+unzip glove.twitter.27B.zip
 cut -d " " -f 2- glove.twitter.27B.100d.txt > glove.txt # strip first column
-rm glove.twitter.27B.100d.txt
+rm glove.twitter.27B.*
