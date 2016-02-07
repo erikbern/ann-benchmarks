98e565a21bad3f427a172a6a58ababde391d74a9
Erik Bernhardsson <erikbern@spotify.com>
2016-02-07 12:34:40 -0500

    fix for kgraph/openblas

--- install/kgraph.sh
+++ install/kgraph.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/aaalgo/kgraph
-apt-get install -y libboost-timer1.48-dev libboost-chrono1.48-dev libboost-program-options1.48-dev libboost-system1.48-dev libopenblas-dev
+apt-get install -y libboost-timer1.48-dev libboost-chrono1.48-dev libboost-program-options1.48-dev libboost-system1.48-dev
+apt-get install -y libopenblas-dev
 apt-get autoremove -y
 
 ls /usr/lib/openblas-base
