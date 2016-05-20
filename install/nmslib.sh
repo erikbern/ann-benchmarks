9b282dda06f076a2292c8ada33de5fbe194a825a
searchivarius <leo@boytsov.info>
2016-05-20 12:34:11 -0400

    Now using NSMLIB 1.5

--- install/nmslib.sh
+++ install/nmslib.sh
@@ -2,8 +2,10 @@ cd "$(dirname "$0")"
 echo "Installing Python interface for the Non-Metric Space Library"
 # Remove the previous version if existed
 rm -rf nmslib 
-# Note that we use the pserv branch here:
-git clone https://github.com/searchivarius/nmslib.git
+# Note that we use version 1.5 here:
+#git clone https://github.com/searchivarius/nmslib.git
+wget https://github.com/searchivarius/nmslib/archive/v1.5.tar.gz
+tar -zxvf v1.5.tar.gz
 cd nmslib/similarity_search
 git checkout pserv
 apt-get install -y cmake libeigen3-dev libgsl0-dev
