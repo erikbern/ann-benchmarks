9eee26afa1329c7fc0e3e67a857499df4bd151f3
Alec Faithfull <alef@itu.dk>
2017-02-01 18:32:04 +0100

    The RPForest algorithm wrapper should actually call the right constructor
    Ahem. The error printing code in get_algorithms should really print a
    backtrace instead of just the message; that'd make stupid mistakes like
    this much more detectable

--- ann_benchmarks/algorithms/rpforest.py
+++ ann_benchmarks/algorithms/rpforest.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import
-from rpforest import RPForest
+import rpforest
 from ann_benchmarks.algorithms.base import BaseANN
 
 class RPForest(BaseANN):
     def __init__(self, leaf_size, n_trees):
         self.name = 'RPForest(leaf_size=%d, n_trees=%d)' % (leaf_size, n_trees)
-        self._model = RPForest(leaf_size=leaf_size, no_trees=n_trees)
+        self._model = rpforest.RPForest(leaf_size=leaf_size, no_trees=n_trees)
 
     def fit(self, X):
         self._model.fit(X)
