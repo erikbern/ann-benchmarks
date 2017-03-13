93bb8e7ad11da65b0a1e2471a8c7e8b3ad017aad
Alec Faithfull <alef@itu.dk>
2017-03-13 18:37:43 +0000

    Converted the dataset download scripts to use the shell function library

--- install/glove.sh
+++
@@ -1,5 +0,0 @@
-cd "$(dirname "$0")"
-wget "https://s3-us-west-1.amazonaws.com/annoy-vectors/glove.twitter.27B.100d.txt.gz"
-gzip -d glove.twitter.27B.100d.txt.gz
-cut -d " " -f 2- glove.twitter.27B.100d.txt > glove.txt # strip first column
-rm glove.twitter.27B.*
