2d5b8e467e543ff120e7a6967ebd8787865d51bd
Alec Faithfull <alef@itu.dk>
2017-03-13 12:41:06 +0000

    Added a shell function library
    There's a lot of duplication in the shell scripts in the install/ folder;
    this is an attempt to start fixing that and tidying it up

---
+++ install/_ins_utilities.sh
@@ -0,0 +1,17 @@
+# ($0: target directory)
+# $1: URL
+# (changes directory to target directory)
+ins_git_get() {
+	dir="${0##*/}"
+	dir="${dir%%.sh}"
+	if [ ! -d "$dir" ]; then
+		git clone "$1" "$dir"
+	else (
+		cd "$dir" &&
+		git clean -dfx &&
+		git reset --hard origin/master &&
+		git pull
+	) fi
+	cd "$dir" &&
+	git apply ../${dir}_*.patch || true
+}
