e0c04c31add421ae91da8f93473d31e429c0787f
Alec Faithfull <alef@itu.dk>
2017-03-14 14:44:30 +0000

    Added a pip wrapper to the shell function library
    This function checks that pip is available before using it to install
    named packages, and will be used to convert the remaining library download
    scripts

--- install/_ins_utilities.sh
+++ install/_ins_utilities.sh
@@ -2,23 +2,27 @@
 # $1: URL
 # (changes directory to target directory)
 ins_git_get() {
-	dir="${0##*/}"
-	dir="${dir%%.sh}"
-	if [ ! -d "$dir" ]; then
-		git clone "$1" "$dir"
-	else (
-		cd "$dir" &&
-		git clean -dfx &&
-		git reset --hard origin/master &&
-		git pull
-	) fi
-	cd "$dir" &&
-	git am ../${dir}_*.patch || true
+  dir="${0##*/}"
+  dir="${dir%%.sh}"
+  if [ ! -d "$dir" ]; then
+    git clone "$1" "$dir"
+  else (
+    cd "$dir" &&
+    git clean -dfx &&
+    git reset --hard origin/master &&
+    git pull
+  ) fi
+  cd "$dir" &&
+  git am ../${dir}_*.patch || true
 }
 
 # $@: package names
 ins_deb_require() {
-	dpkg-query --show "$@"
+  dpkg-query --show "$@"
+}
+
+ins_pip_get() {
+  ins_deb_require python-pip && pip install "$@"
 }
 
 # $1: URL of file containing gzipped dataset
