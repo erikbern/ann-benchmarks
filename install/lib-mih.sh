41156aa12a97e94e52f17c66fd8aba5f655a4612
Martin Aumueller <maau@itu.dk>
2017-03-28 20:51:53 +0000

    Renamed wrapper file. Copy all the files in place and apply patch to build wrapper automatically.

--- install/lib-mih.sh
+++ install/lib-mih.sh
@@ -5,10 +5,11 @@ cd "$(dirname "$0")"
 ins_deb_require cmake libhdf5-dev &&
   ins_git_get https://github.com/norouzi/mih &&
   mkdir build &&
+  mkdir wrapper &&
+  cp ../lib-annitu/wrappers/frontend/frontend.c wrapper/ &&
+  cp ../lib-mih-wrapper.cpp wrapper/ &&
   cd build &&
   cmake ../ &&
-  make -j4 &&
-  gcc -O3 -c -o frontend.o ../lib-annitu/wrappers/frontend/frontend.c &&
-  g++ -O3 -std=c++14 -o fr-mih frontend.o ../lib-mih.cpp -pthread
+  make -j4
 
 
