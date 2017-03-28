b450809b56b8260b56fb93625d12136d24d219dc
Martin Aumueller <maau@itu.dk>
2017-03-28 15:01:54 +0000

    Included mih.

--- install/lib-mih.sh
+++ install/lib-mih.sh
@@ -7,4 +7,8 @@ ins_deb_require cmake libhdf5-dev &&
   mkdir build &&
   cd build &&
   cmake ../ &&
-  make -j4
+  make -j4 &&
+  gcc -O3 -c -o frontend.o ../lib-annitu/wrappers/frontend/frontend.c &&
+  g++ -O3 -std=c++14 -o fr-mih frontend.o ../lib-mih.cpp -pthread
+
+
