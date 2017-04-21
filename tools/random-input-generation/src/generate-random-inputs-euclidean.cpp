0612f028225684433dc18462b0640c64ce43b916
Martin Aumueller <maau@itu.dk>
2017-04-21 10:07:43 +0000

    Small changes in random input generation.

--- tools/random-input-generation/src/generate-random-inputs-euclidean.cpp
+++ tools/random-input-generation/src/generate-random-inputs-euclidean.cpp
@@ -1,5 +1,5 @@
 /**
- * @file generate-random-inputs-unit-sphere.cpp 
+ * @file generate-random-inputs-euclidean.cpp
  * @author Martin Aumüller
  *
  */
@@ -17,20 +17,39 @@ std::random_device rd;
 std::mt19937_64 gen(rd());
 std::normal_distribution<double> distribution(0.0, 1.0);
 
-using vecType = std::vector<double>; 
+using vecType = std::vector<double>;
 
-vecType generateRandomVector(size_t d) {
+double length(const vecType& v) {
+    double t = 0.0;
+    for (auto e: v) {
+        t += e * e;
+    }
+    return sqrt(t);
+}
+
+void normalize(vecType& v) {
+    auto len = length(v);
+    for (size_t i = 0; i < v.size(); i++) {
+        v[i] /= len;
+    }
+}
+
+vecType generateRandomVector(size_t d, bool unit) {
     vecType v(d);
-    for (size_t i = 0; i < d; i++) 
+    for (size_t i = 0; i < d; i++)
         v[i] = distribution(gen) / sqrt(d);
+    if (unit)
+        normalize(v);
     return v;
 }
 
-vecType distortPoint(const vecType& v, float targetDistance) {
+vecType distortPoint(const vecType& v, float targetDistance, bool unit) {
     size_t d = v.size();
-    vecType w(d); 
+    vecType w(d);
     for (size_t i = 0; i < d; i++)
         w[i] = v[i] + distribution(gen) * targetDistance/sqrt(d);
+    if (unit)
+        normalize(w);
     return w;
 }
 
@@ -47,20 +66,18 @@ void writeFile(std::string filename, std::set<T>& set) {
 	outputFile.close();
 }
 
-int main(int argc, char** argv)
-{
-
+int main(int argc, char** argv) {
 	TCLAP::CmdLine cmd("This program generates random inputs in Euclidean space.", ' ', "0.9");
 
 	TCLAP::ValueArg<size_t> numberOfPointsArg("n", "numpoints", "Total number of points", true, 10, "integer");
 	TCLAP::ValueArg<size_t> numberOfDimensionsArg("d", "dimension", "Number of dimensions", true, 15, "integer");
 	TCLAP::ValueArg<size_t> numberOfClusters("c", "numclusters", "Number of clusters", true, 15, "integer");
 	TCLAP::ValueArg<size_t> pointsPerCluster("p", "pointscluster", "Number of points per cluster", true, 15, "integer");
-
 	TCLAP::ValueArg<std::string> outputFileArg("o", "outputfile", "File the output should be written to. ", true, "input.txt", "string");
 	TCLAP::ValueArg<std::string> queryOutFileArg("q", "queryoutfile", "File the query set should be written to. ", true, "input.txt", "string");
     TCLAP::ValueArg<size_t> seedArg("s", "seed", "Seed for random generator", false, 1234, "integer");
     TCLAP::SwitchArg randomQueryArg("g", "randomqueries", "Should random query points (without close neighbors) be added?");
+    TCLAP::SwitchArg normalizeArg("N", "normalize", "Normalize vectors to unit length?");
 
     cmd.add(numberOfPointsArg);
     cmd.add(numberOfDimensionsArg);
@@ -70,6 +87,7 @@ int main(int argc, char** argv)
     cmd.add(queryOutFileArg);
     cmd.add(seedArg);
     cmd.add(randomQueryArg);
+    cmd.add(normalizeArg);
 
 	cmd.parse(argc, argv);
 
@@ -78,17 +96,17 @@ int main(int argc, char** argv)
     auto numClusters = numberOfClusters.getValue();
     auto numPoints = pointsPerCluster.getValue();
     size_t numNoisePoints = 2 * numPoints;
+    bool unit = normalizeArg.isSet();
 
-    if (seedArg.isSet())
-    {
+    if (seedArg.isSet()) {
        gen.seed(seedArg.getValue());
     }
 
 	std::set<vecType> set;
-    std::set<vecType> querySet; 
+    std::set<vecType> querySet;
 
     for (size_t i = 0; i < numClusters; i++) {
-        querySet.insert(generateRandomVector(d));
+        querySet.insert(generateRandomVector(d, unit));
     }
 
     std::cout << "Generated " << querySet.size() << " clusters ... " << std::endl;
@@ -98,42 +116,38 @@ int main(int argc, char** argv)
     int curQuery = 0;
 
     for (auto& q: querySet) {
-
         curQuery++;
         double targetDistance = curQuery * windowSize;
 
         // Compute points at target distance around query.
         for (size_t i = 0; i < numPoints; i++) {
-            set.insert(distortPoint(q, targetDistance));
+            set.insert(distortPoint(q, targetDistance, unit));
         }
         // Compute some noise.
         targetDistance *= 2;
         for (size_t i = 0; i < numNoisePoints; i++) {
-            set.insert(distortPoint(q, targetDistance));
+            set.insert(distortPoint(q, targetDistance, unit));
         }
     }
 
     // Add random query points
     if (randomQueryArg.isSet()) {
         for (size_t i = 0; i < numClusters; i++) {
-            querySet.insert(generateRandomVector(d));
+            querySet.insert(generateRandomVector(d, unit));
         }
     }
 
     std::cout << "Created clusters with a total number of " << set.size() << " points." << std::endl;
-
-
 	std::cout << "Fill with random points..." << std::endl;
 
 	// Fill with random points
     while (set.size() < n) {
-        set.insert(generateRandomVector(d));
+        set.insert(generateRandomVector(d, unit));
 	}
 
     std::cout << "Writing result to file" << std::endl;
-    
-    writeFile(outputFileArg.getValue(), set); 
-    writeFile(queryOutFileArg.getValue(), querySet); 
+    writeFile(outputFileArg.getValue(), set);
+    writeFile(queryOutFileArg.getValue(), querySet);
 
 	return 0;
 
