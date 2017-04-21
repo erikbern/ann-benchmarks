058cd2cea55685df6b771bb4b2030c66aec94f53
Martin Aumueller <a@a.com>
2017-04-21 09:12:51 +0000

    Use up-to-date version of frontend.

--- install/frontend.c
+++ install/frontend.c
@@ -46,8 +46,21 @@ char** tokenise(
         while (subloop && *pos && (c1 = *(pos++))) {
           switch (c1) {
             case '\\':
-              if (pos != end)
-                update_buf(*(pos++), &buf, &buflen, &bufpos);
+              if (*pos) {
+                c1 = *(pos++);
+                switch (c1) {
+                  case '$':
+                    /* fall through */
+                  case '\\':
+                    /* fall through */
+                  case '\"':
+                    update_buf(c1, &buf, &buflen, &bufpos);
+                    break;
+                  default:
+                    update_buf('\\', &buf, &buflen, &bufpos);
+                    update_buf((char)c1, &buf, &buflen, &bufpos);
+                }
+              }
               break;
             case '\"':
               subloop = false;
@@ -120,14 +133,32 @@ extern bool train(const char* entry);
    training data.) */
 extern void end_train(void);
 /* Searches for @count close matches to @entry, returning the number of matches
-   found; the query_result function should be called this many times to return
-   each match. */
+   found; the query_result function will be called this many times (but no
+   more than @count times) to retrieve those matches.
+
+   (When prepared query mode is enabled and supported, @entry will be NULL.) */
 extern size_t query(const char* entry, size_t count);
-/* Returns the next query result, or SIZE_MAX if there are none remaining. */
+/* Returns the next query result, or SIZE_MAX if there are none remaining.
+   Closer matches should be returned first. */
 extern size_t query_result(void);
 /* Called immediately before finishing query mode and terminating the
    program. */
 extern void end_query(void);
+
+/* Support for prepared query mode. */
+
+/* Prepares the query represented by @entry for execution. Returns true if the
+   underlying strategy supports prepared query mode (even if @entry does not
+   represent a valid query!), or false otherwise. */
+extern bool prepare_query(const char* entry);
+
+bool __attribute__((weak)) prepare_query(const char* entry) {
+  return false;
+}
+
+static size_t min(size_t a, size_t b) {
+  return (a < b ? a : b);
+}
 
 int main(int argc, const char* argv[]) {
   setvbuf(stdin, NULL, _IOLBF, 0);
@@ -138,11 +169,18 @@ int main(int argc, const char* argv[]) {
   size_t buflen = 0;
   ssize_t length = 0;
 
+  bool prepared_queries = false;
+
   /* Configuration mode */
   while ((length = getline(&buf, &buflen, stdin)) > 1) {
     char** tokens = tokenise(buf, length, &token_count);
     if (token_count == 2) {
       if (configure(tokens[0], tokens[1])) {
+        printf("epbprtv0 ok\n");
+      } else printf("epbprtv0 fail\n");
+    } else if (token_count == 3 && strcmp(tokens[0], "frontend") == 0) {
+      if (strcmp(tokens[1], "prepared-queries") == 0) {
+        prepared_queries = (strcmp(tokens[2], "1") == 0);
         printf("epbprtv0 ok\n");
       } else printf("epbprtv0 fail\n");
     } else printf("epbprtv0 fail\n");
@@ -173,28 +211,65 @@ int main(int argc, const char* argv[]) {
     printf(" fail %u", fail);
   putchar('\n');
 
-  /* Query mode */
-  while ((length = getline(&buf, &length, stdin)) > 1) {
-    char** tokens = tokenise(buf, length, &token_count);
-    if (token_count == 2) {
-      char* count_token = tokens[1];
-      char* last;
-      long int k = 0;
-      errno = 0;
-      k = strtol(count_token, &last, 10);
-      if (errno != 0 || *count_token == 0 || *last != 0 || k < 1) {
-        printf("epbprtv0 fail\n");
-      } else {
-        size_t result_count = 0;
-        size_t* results = NULL;
-        if ((result_count = query(tokens[0], k)) > 0) {
-          printf("epbprtv0 ok %u\n", result_count);
-          for (size_t i = 0; i < result_count; i++)
-            printf("epbprtv0 %u\n", query_result());
+  if (!prepared_queries) {
+    /* Query mode */
+    while ((length = getline(&buf, &length, stdin)) > 1) {
+      char** tokens = tokenise(buf, length, &token_count);
+      if (token_count == 2) {
+        char* count_token = tokens[1];
+        char* last;
+        long int k = 0;
+        errno = 0;
+        k = strtol(count_token, &last, 10);
+        if (errno != 0 || *count_token == 0 || *last != 0 || k < 1) {
+          printf("epbprtv0 fail\n");
+        } else {
+          size_t result_count = 0;
+          if ((result_count = query(tokens[0], k)) > 0) {
+            size_t to_return = min(k, result_count);
+            printf("epbprtv0 ok %u total_candidates %u\n", to_return, result_count);
+            for (size_t i = 0; i < to_return; i++)
+              printf("epbprtv0 %u\n", query_result());
+          } else printf("epbprtv0 fail\n");
+        }
+      } else printf("epbprtv0 fail\n");
+      tokenise_free(tokens);
+    }
+  } else {
+    /* Prepared query mode */
+    char* last_query = NULL;
+    long int last_k = 0;
+    while ((length = getline(&buf, &length, stdin)) > 1) {
+      char** tokens = tokenise(buf, length, &token_count);
+      if (token_count == 1 && strcmp(tokens[0], "query") == 0) {
+        if (last_k) {
+          size_t result_count = 0;
+          if ((result_count = query(last_query, last_k)) > 0) {
+            size_t to_return = min(last_k, result_count);
+            printf("epbprtv0 ok %u total_candidates %u\n", to_return, result_count);
+            for (size_t i = 0; i < to_return; i++)
+              printf("epbprtv0 %u\n", query_result());
+          } else printf("epbprtv0 fail\n");
         } else printf("epbprtv0 fail\n");
-      }
-    } else printf("epbprtv0 fail\n");
-    tokenise_free(tokens);
+      } else if (token_count == 2) {
+        char* count_token = tokens[1];
+        char* last;
+        long int k = 0;
+        errno = 0;
+        k = strtol(count_token, &last, 10);
+        if (errno != 0 || *count_token == 0 || *last != 0 || k < 1) {
+          printf("epbprtv0 fail\n");
+        } else {
+          if (last_query)
+            free(last_query);
+          bool prepared = prepare_query(tokens[0]);
+          last_query = (prepared ? NULL : strdup(tokens[0]));
+          last_k = k;
+          printf("epbprtv0 ok prepared %s\n", (prepared ? "true" : "false"));
+        }
+      } else printf("epbprtv0 fail\n");
+      tokenise_free(tokens);
+    }
   }
   end_query();
   free(buf);
