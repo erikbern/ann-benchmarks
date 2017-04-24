4d1daa81038be5d7c9a75269852b2de9505d8eb5
Alec Faithfull <alef@itu.dk>
2017-04-24 15:46:48 +0200

    Removed an erroneous parameter in main.py's call to get_definitions
    Commit 28bbde9cb9548915d5f5638d4134e0ace3d09213 was going to add a new
    "@count" special value to the definitions file, which would have to be
    passed to get_definitions, but that was removed after after the changes
    to NearPy made it unnecessary. This was a leftover that didn't get caught

--- ann_benchmarks/main.py
+++ ann_benchmarks/main.py
@@ -316,7 +316,7 @@ error: the training dataset and query dataset have incompatible manifests"""
 
     point_type = manifest['point_type']
     algos = get_algorithms(definitions, constructors,
-        len(X_train[0]), point_type, args.distance, args.count)
+        len(X_train[0]), point_type, args.distance)
 
     if args.algorithm:
         print('running only', args.algorithm)
