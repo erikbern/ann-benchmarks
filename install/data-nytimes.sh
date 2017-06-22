d4fcd0e6c82283a346d85dae2a682331626213cf
Martin Aumueller <maau@itu.dk>
2017-06-22 12:48:23 +0000

    Fixed installation script for NYTimes dataset.

--- install/data-nytimes.sh
+++ install/data-nytimes.sh
@@ -3,11 +3,9 @@ cd "$(dirname "$0")"
 . ./_ins_utilities.sh
 
 if [ ! -f "nytimes.tfidf.256.data.txt" ]; then
-    if [ ! -f "nytimes.tifidf.256.data.tar.bz2" ]; then
-        wget http://www.itu.dk/people/maau/ann-benchmark-datasets/nytimes.tifidf.256.data.tar.bz2
+    if [ ! -f "nytimes.tfidf.256.tar.bz2" ]; then
+        wget http://www.itu.dk/people/maau/ann-benchmark-datasets/nytimes.tfidf.256.tar.bz2
     fi
 
-    tar xf nytimes.tifidf.256.data.tar.bz2
-END
-
-
+    tar xf nytimes.tfidf.256.tar.bz2
+fi
