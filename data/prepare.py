3d303775df3eb17a5d5f18f6eb1854340277746e
Erik Bernhardsson <mail@erikbern.com>
2017-11-11 12:26:57 -0500

    mnist

--- data/prepare.py
+++ data/prepare.py
@@ -1,3 +1,4 @@
+import gzip
 import h5py
 import numpy
 import os
@@ -29,18 +30,18 @@ def write_output(train, test, fn, distance, count=100):
     queries = []
     for i, x in enumerate(test):
         if i % 1000 == 0:
-            print('%d...' % i)
+            print('%d/%d...' % (i, test.shape[0]))
         correct[i] = [j for j, _ in bf.query_with_distances(x, count)]
     f.close()
 
 
-def glove(tag, d):
+def glove(out_fn, d):
     url = 'http://nlp.stanford.edu/data/glove.twitter.27B.zip'
     fn = os.path.join('data', 'glove.twitter.27B.zip')
     download(url, fn)
     with zipfile.ZipFile(fn) as z:
         random.seed(1)
-        print('parsing %s' % tag)
+        print('parsing %s' % out_fn)
         z_fn = 'glove.twitter.27B.%dd.txt' % d
         train = []
         test = []
@@ -54,10 +55,10 @@ def glove(tag, d):
         print('writing output...')
         train = numpy.array(train)
         test = numpy.array(test)
-        write_output(train, test, '%s.hdf5' % tag, 'angular')
+        write_output(train, test, out_fn, 'angular')
 
 
-def sift(tag, ):
+def sift(out_fn):
     url = 'ftp://ftp.irisa.fr/local/texmex/corpus/sift.tar.gz'
     fn = os.path.join('data', 'sift.tar.tz')
     download(url, fn)
@@ -77,19 +78,56 @@ def sift(tag, ):
             
         train = load_vectors('sift/sift_base.fvecs')
         test = load_vectors('sift/sift_query.fvecs')
-        write_output(train, test, '%s.hdf5' % tag, 'euclidean')
+        write_output(train, test, out_fn, 'euclidean')
+
+
+def mnist(out_fn):
+    download('http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz', 'mnist-train.gz')
+    download('http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz', 'mnist-test.gz')
+    def load_vectors(fn):
+        print('parsing vectors in %s...' % fn)
+        f = gzip.open(fn)
+        type_code_info = {
+            0x08: (1, "!B"),
+            0x09: (1, "!b"),
+            0x0B: (2, "!H"),
+            0x0C: (4, "!I"),
+            0x0D: (4, "!f"),
+            0x0E: (8, "!d")
+        }
+        magic, type_code, dim_count = struct.unpack("!hBB", f.read(4))
+        assert magic == 0
+        assert type_code in type_code_info
+
+        dimensions = [struct.unpack("!I", f.read(4))[0] for i in range(dim_count)]
+
+        entry_count = dimensions[0]
+        entry_size = numpy.product(dimensions[1:])
+
+        b, format_string = type_code_info[type_code]
+        vectors = []
+        for i in range(entry_count):
+            vectors.append([struct.unpack(format_string, f.read(b))[0] for j in range(entry_size)])
+        return numpy.array(vectors)
+
+    train = load_vectors('mnist-train.gz')
+    test = load_vectors('mnist-test.gz')
+    write_output(train, test, out_fn, 'euclidean')
 
 
 datasets = {
-    'sift-128-euclidean': sift,
     'glove-25-angular': lambda tag: glove(tag, 25),
     'glove-50-angular': lambda tag: glove(tag, 50),
     'glove-100-angular': lambda tag: glove(tag, 100),
     'glove-200-angular': lambda tag: glove(tag, 200),
+    'mnist-784-euclidean': mnist,
+    'sift-128-euclidean': sift,
 }
+
 
 if __name__ == '__main__':
     tag = sys.argv[1]
     if tag not in datasets:
         raise Exception('%s has to be one of %s' % (tag, ','.join(datasets.keys())))
-    datasets[tag](tag)
+    fn = os.path.join('data', '%s.hdf5' % tag)
+    datasets[tag](fn)
