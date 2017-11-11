b4a908b03eed3ffc8bed8f716f1c4df7e451b40a
Erik Bernhardsson <mail@erikbern.com>
2017-11-11 14:02:34 -0500

    fashion-mnist, gist

--- data/README.md
+++ data/README.md
@@ -11,11 +11,13 @@ Prepares a bunch of datasets
 Available datasets
 ------------------
 
-| Dataset | Dimensions | Train size | Test size | Neighbors | Distance  | URL                                                  |
-| ------- | ---------: | ---------: | --------: | --------: | --------- | ---------------------------------------------------- |
-| Glove   |         25 |    954,623 |   238,891 |       100 | Angular   | http://vectors.erikbern.com/glove-25-angular.hdf5    |
-| Glove   |         50 |            |           |       100 | Angular   | http://vectors.erikbern.com/glove-50-angular.hdf5    |
-| Glove   |        100 |            |           |       100 | Angular   | http://vectors.erikbern.com/glove-100-angular.hdf5   |
-| Glove   |        200 |            |           |       100 | Angular   | http://vectors.erikbern.com/glove-200-angular.hdf5   |
-| MNIST   |        784 |     60,000 |    10,000 |       100 | Euclidean | http://vectors.erikbern.com/mnist-784-euclidean.hdf5 |
-| SIFT    |        128 |  1,000,000 |    10,000 |       100 | Euclidean | http://vectors.erikbern.com/sift-100-euclidean.hdf5  |
+| Dataset         | Dimensions | Train size | Test size | Neighbors | Distance  | URL                                                          |
+| --------------- | ---------: | ---------: | --------: | --------: | --------- | ------------------------------------------------------------ |
+| Fashion-MNIST   |        784 |     60,000 |    10,000 |       100 | Euclidean | http://vectors.erikbern.com/fashion-mnist-784-euclidean.hdf5 |
+| GIST            |        960 |  1,000,000 |     1,000 |       100 | Euclidean | http://vectors.erikbern.com/gist-960-euclidean.hdf5          |
+| Glove           |         25 |  1,133,628 |    59,886 |       100 | Angular   | http://vectors.erikbern.com/glove-25-angular.hdf5            |
+| Glove           |         50 |  1,133,628 |    59,886 |       100 | Angular   | http://vectors.erikbern.com/glove-50-angular.hdf5            |
+| Glove           |        100 |  1,133,628 |    59,886 |       100 | Angular   | http://vectors.erikbern.com/glove-100-angular.hdf5           |
+| Glove           |        200 |  1,133,628 |    59,886 |       100 | Angular   | http://vectors.erikbern.com/glove-200-angular.hdf5           |
+| MNIST           |        784 |     60,000 |    10,000 |       100 | Euclidean | http://vectors.erikbern.com/mnist-784-euclidean.hdf5         |
+| SIFT            |        128 |  1,000,000 |    10,000 |       100 | Euclidean | http://vectors.erikbern.com/sift-128-euclidean.hdf5          |
--- data/prepare.py
+++ data/prepare.py
@@ -16,12 +16,15 @@ from ann_benchmarks.algorithms.bruteforce import BruteForceBLAS
 def download(src, dst):
     if not os.path.exists(dst):
         # TODO: should be atomic
+        print('downloading %s -> %s...' % (src, dst))
         urllib.request.urlretrieve(src, dst)
 
 
 def write_output(train, test, fn, distance, count=100):
     n = 0
     f = h5py.File(fn, 'w')
+    print('train size: %d * %d' % train.shape)
+    print('test size: %d * %d' % test.shape)
     f.create_dataset('train', (len(train), len(train[0])), dtype='f')[:] = train
     f.create_dataset('test', (len(test), len(test[0])), dtype='f')[:] = test
     correct = f.create_dataset('correct', (len(test), count), dtype='i')
@@ -41,13 +44,13 @@ def glove(out_fn, d):
     download(url, fn)
     with zipfile.ZipFile(fn) as z:
         random.seed(1)
-        print('parsing %s' % out_fn)
+        print('preparing %s' % out_fn)
         z_fn = 'glove.twitter.27B.%dd.txt' % d
         train = []
         test = []
         for line in z.open(z_fn):
             v = [float(x) for x in line.strip().split()[1:]]
-            if random.randint(0, 4):
+            if random.randint(0, 19):
                 train.append(v)
             else:
                 test.append(v)
@@ -58,64 +61,84 @@ def glove(out_fn, d):
         write_output(train, test, out_fn, 'angular')
 
 
+def _load_texmex_vectors(f):
+    vs = []
+    while True:
+        b = f.read(4)
+        if not b:
+            break
+        dim = struct.unpack('i', b)[0]
+        vec = struct.unpack('f' * dim, f.read(dim*4))
+        vs.append(vec)
+    return numpy.array(vs)
+
+
 def sift(out_fn):
     url = 'ftp://ftp.irisa.fr/local/texmex/corpus/sift.tar.gz'
     fn = os.path.join('data', 'sift.tar.tz')
     download(url, fn)
     with tarfile.open(fn, 'r:gz') as t:
-        def load_vectors(t_fn):
-            print('reading %s...' % t_fn)
-            vs = []
-            f = t.extractfile(t.getmember(t_fn))
-            while True:
-                b = f.read(4)
-                if not b:
-                    break
-                dim = struct.unpack('i', b)[0]
-                vec = struct.unpack('f' * dim, f.read(dim*4))
-                vs.append(vec)
-            return numpy.array(vs)
-            
-        train = load_vectors('sift/sift_base.fvecs')
-        test = load_vectors('sift/sift_query.fvecs')
+        train = _load_texmex_vectors(t.extractfile(t.getmember('sift/sift_base.fvecs')))
+        test = _load_texmex_vectors(t.extractfile(t.getmember('sift/sift_query.fvecs')))
         write_output(train, test, out_fn, 'euclidean')
+
+
+def gist(out_fn):
+    url = 'ftp://ftp.irisa.fr/local/texmex/corpus/gist.tar.gz'
+    fn = os.path.join('data', 'gist.tar.tz')
+    download(url, fn)
+    with tarfile.open(fn, 'r:gz') as t:
+        train = _load_texmex_vectors(t.extractfile(t.getmember('gist/gist_base.fvecs')))
+        test = _load_texmex_vectors(t.extractfile(t.getmember('gist/gist_query.fvecs')))
+        write_output(train, test, out_fn, 'euclidean')
+
+
+def _load_mnist_vectors(fn):
+    print('parsing vectors in %s...' % fn)
+    f = gzip.open(fn)
+    type_code_info = {
+        0x08: (1, "!B"),
+        0x09: (1, "!b"),
+        0x0B: (2, "!H"),
+        0x0C: (4, "!I"),
+        0x0D: (4, "!f"),
+        0x0E: (8, "!d")
+    }
+    magic, type_code, dim_count = struct.unpack("!hBB", f.read(4))
+    assert magic == 0
+    assert type_code in type_code_info
+
+    dimensions = [struct.unpack("!I", f.read(4))[0] for i in range(dim_count)]
+
+    entry_count = dimensions[0]
+    entry_size = numpy.product(dimensions[1:])
+
+    b, format_string = type_code_info[type_code]
+    vectors = []
+    for i in range(entry_count):
+        vectors.append([struct.unpack(format_string, f.read(b))[0] for j in range(entry_size)])
+    return numpy.array(vectors)
 
 
 def mnist(out_fn):
     download('http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz', 'mnist-train.gz')
     download('http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz', 'mnist-test.gz')
-    def load_vectors(fn):
-        print('parsing vectors in %s...' % fn)
-        f = gzip.open(fn)
-        type_code_info = {
-            0x08: (1, "!B"),
-            0x09: (1, "!b"),
-            0x0B: (2, "!H"),
-            0x0C: (4, "!I"),
-            0x0D: (4, "!f"),
-            0x0E: (8, "!d")
-        }
-        magic, type_code, dim_count = struct.unpack("!hBB", f.read(4))
-        assert magic == 0
-        assert type_code in type_code_info
+    train = _load_mnist_vectors('mnist-train.gz')
+    test = _load_mnist_vectors('mnist-test.gz')
+    write_output(train, test, out_fn, 'euclidean')
 
-        dimensions = [struct.unpack("!I", f.read(4))[0] for i in range(dim_count)]
 
-        entry_count = dimensions[0]
-        entry_size = numpy.product(dimensions[1:])
-
-        b, format_string = type_code_info[type_code]
-        vectors = []
-        for i in range(entry_count):
-            vectors.append([struct.unpack(format_string, f.read(b))[0] for j in range(entry_size)])
-        return numpy.array(vectors)
-
-    train = load_vectors('mnist-train.gz')
-    test = load_vectors('mnist-test.gz')
+def fashion_mnist(out_fn):
+    download('http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz', 'fashion-mnist-train.gz')
+    download('http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz', 'fashion-mnist-test.gz')
+    train = _load_mnist_vectors('fashion-mnist-train.gz')
+    test = _load_mnist_vectors('fashion-mnist-test.gz')
     write_output(train, test, out_fn, 'euclidean')
 
 
 datasets = {
+    'fashion-mnist-784-euclidean': fashion_mnist,
+    'gist-960-euclidean': gist,
     'glove-25-angular': lambda tag: glove(tag, 25),
     'glove-50-angular': lambda tag: glove(tag, 50),
     'glove-100-angular': lambda tag: glove(tag, 100),
