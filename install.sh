9aeb2d7594bc4f669422027f54aed6e72f63a1be
Erik Bernhardsson <mail@erikbern.com>
2017-12-15 20:34:46 -0500

    use python script instead of bash for installing images, build all images by default

--- install.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-if [ -z $LIBRARY ]
-then
-    LIBRARY=$1
-fi
-
-docker build -t ann-benchmarks -f install/Dockerfile .
-docker build -t ann-benchmarks-$LIBRARY -f install/Dockerfile.$LIBRARY . 
