b0441853bb94db4e37fb8bb27cfc634aa10b391f
Erik Bernhardsson <mail@erikbern.com>
2018-05-07 23:13:48 -0400

    Add MRPT

---
+++ ann_benchmarks/algorithms/mrpt.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+import mrpt
+from ann_benchmarks.algorithms.base import BaseANN
+
+class MRPT(BaseANN):
+    def __init__(self, n_trees, depth):
+        self._n_trees = n_trees
+        self._depth = depth
+        self._votes_required = None
+
+    def fit(self, X):
+        self._index = mrpt.MRPTIndex(X, depth=self._depth, n_trees=self._n_trees)
+        self._index.build()
+
+    def set_query_arguments(self, votes_required):
+        self._votes_required = votes_required
+
+    def query(self, v, n):
+        return self._index.ann(v, n, votes_required=self._votes_required)
+
+    def __str__(self):
+        return 'MRPT(n_trees=%d, depth=%d, votes_required=%d)' % (self._n_trees, self._depth, self._votes_required)
