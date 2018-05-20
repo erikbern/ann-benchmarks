b564957e7b9a40397ed73919a6c6c862a6b2d5ce
Ubuntu <ubuntu@ip-172-31-3-207.us-west-2.compute.internal>
2018-05-20 15:10:17 +0000

    fix angular distance for mrpt

--- ann_benchmarks/algorithms/mrpt.py
+++ ann_benchmarks/algorithms/mrpt.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+import sklearn.preprocessing
 import mrpt
 from ann_benchmarks.algorithms.base import BaseANN
 
@@ -9,6 +10,9 @@ class MRPT(BaseANN):
         self._votes_required = None
 
     def fit(self, X):
+        if self._metric == 'angular':
+            X = sklearn.preprocessing.normalize(X, axis=1, norm='l2')
+
         self._index = mrpt.MRPTIndex(X, depth=self._depth, n_trees=self._n_trees)
         self._index.build()
 
@@ -16,6 +20,9 @@ class MRPT(BaseANN):
         self._votes_required = votes_required
 
     def query(self, v, n):
+        if self._metric == 'angular':
+            v /= numpy.linalg.norm(v)
+
         return self._index.ann(v, n, votes_required=self._votes_required)
 
     def __str__(self):
