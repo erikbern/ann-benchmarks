a51e5b80e481aa4344f312cafd83c0a0ee739808
johan.park <johan.park@kakaocorp.com>
2018-07-10 15:36:37 +0900

    Add multiprocessing for building multiple docker images simultaneously
    - add --proc option for setting multiprocessing pool to build docker
    images faster
    - add --algorithm option like LIBRARY environment
    - remain LIBRARY but --algorithm option has higher priority

--- install.py
+++ install.py
@@ -1,17 +1,59 @@
 import json
 import os
+import argparse
 import subprocess
+from multiprocessing import Pool
 
-print('Building base image...')
-subprocess.check_call('docker build --rm -t ann-benchmarks -f install/Dockerfile .', shell=True)
+
+def positive_int(s):
+    i = None
+    try:
+        i = int(s)
+    except ValueError:
+        pass
+    if not i or i < 1:
+        raise argparse.ArgumentTypeError("%r is not a positive integer" % s)
+    return i
+
 
 def build(library):
     print('Building %s...' % library)
-    subprocess.check_call('docker build --rm -t ann-benchmarks-%s -f install/Dockerfile.%s .' % (library, library), shell=True)
+    subprocess.check_call(
+        'docker build \
+        --rm -t ann-benchmarks-%s -f install/Dockerfile.%s .' % (library, library), shell=True)
 
-if os.getenv('LIBRARY'):
-    build(os.getenv('LIBRARY'))
-else:
-    for fn in os.listdir('install'):
-        if fn.startswith('Dockerfile.'):
-            build(fn.split('.')[-1])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        "--proc",
+        default=4,
+        type=positive_int,
+        help="the number of process to build docker images")
+    parser.add_argument(
+        '--algorithm',
+        metavar='NAME',
+        help='build only the named algorithm image',
+        default=None)
+    args = parser.parse_args()
+
+    print('Building base image...')
+    subprocess.check_call(
+        'docker build \
+        --rm -t ann-benchmarks -f install/Dockerfile .', shell=True)
+
+    if args.algorithm:
+        print('Building algorithm(%s) image...' % args.algorithm)
+        build(args.algorithm)
+    elif os.getenv('LIBRARY'):
+        print('Building algorithm(%s) image...' % os.getenv('LIBRARY'))
+        build(os.getenv('LIBRARY'))
+    else:
+        print('Building algorithm images... with (%d) processes' % args.proc)
+        pool = Pool(processes=args.proc)
+        dockerfiles = []
+        for fn in os.listdir('install'):
+            if fn.startswith('Dockerfile.'):
+                dockerfiles.append(fn.split('.')[-1])
+        pool.map(build, dockerfiles)
