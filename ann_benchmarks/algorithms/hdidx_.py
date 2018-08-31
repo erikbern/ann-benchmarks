f8d35d291976e5d2280d604edbb64349a67d7981
Anthony <aagnone@pindrop.com>
2018-08-31 19:24:45 -0400

    Adding basic spectral hash implementation from hdidx

---
+++ ann_benchmarks/algorithms/hdidx_.py
@@ -0,0 +1,27 @@
+import numpy as np
+from hdidx.indexer import SHIndexer
+from ann_benchmarks.algorithms.base import BaseANN
+
+
+class HdIdx(BaseANN):
+    def __init__(self, index):
+        self._index = index
+        self._index.set_storage()  # defaults to main memory
+        self._params = dict()  # set by sub-classes
+
+    def fit(self, X):
+        self._params['vals'] = X
+        self._index.build(self._params)
+        self._index.add(X)
+
+    def query(self, v, n):
+        return self._index.search(np.expand_dims(v, axis=0), n)[0][0].tolist()
+
+
+class SHIndexer_(HdIdx):
+    def __init__(self, n_bits=256):
+        super(SHIndexer_, self).__init__(SHIndexer())
+        self._params['nbits'] = n_bits
+
+    def __str__(self):
+        return 'SHIndexer_({})'.format(self._params)
