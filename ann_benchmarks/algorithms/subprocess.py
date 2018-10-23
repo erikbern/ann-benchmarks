b23d289c9d3e4ce87fb92d48aa320effa89eaa08
Alec Faithfull <alef@itu.dk>
2018-10-23 13:18:59 +0200

    Moved the Subprocess prepared query implementation into a new subclass

--- ann_benchmarks/algorithms/subprocess.py
+++ ann_benchmarks/algorithms/subprocess.py
@@ -13,23 +13,29 @@ class SubprocessStoppedError(Exception):
         self.code = code
 
 class Subprocess(BaseANN):
-    def __raw_line(self):
+    def _raw_line(self):
         return shlex.split( \
-            self.__get_program_handle().stdout.readline().strip())
-    def __line(self):
-        line = self.__raw_line()
+            self._get_program_handle().stdout.readline().strip())
+    def _line(self):
+        line = self._raw_line()
+#       print("<- %s" % (" ".join(line)))
         while len(line) < 1 or line[0] != "epbprtv0":
-            line = self.__raw_line()
+            line = self._raw_line()
         return line[1:]
 
     @staticmethod
-    def __quote(token):
+    def _quote(token):
         return "'" + str(token).replace("'", "'\\'") + "'"
 
-    def __write(self, string):
-        self.__get_program_handle().stdin.write(string + "\n")
+    def _write(self, string):
+#       print("-> %s" % string)
+        self._get_program_handle().stdin.write(string + "\n")
 
-    def __get_program_handle(self):
+    # Called immediately before transitioning from query mode to training mode
+    def _configuration_hook(self):
+        pass
+
+    def _get_program_handle(self):
         if self._program:
             self._program.poll()
             if self._program.returncode:
@@ -43,103 +49,63 @@ class Subprocess(BaseANN):
                 universal_newlines = True)
 
             for key, value in iter(self._params.items()):
-                self.__write("%s %s" % \
-                    (Subprocess.__quote(key), Subprocess.__quote(value)))
-                assert self.__line()[0] == "ok", """\
+                self._write("%s %s" % \
+                    (Subprocess._quote(key), Subprocess._quote(value)))
+                assert self._line()[0] == "ok", """\
 assigning value '%s' to option '%s' failed""" % (value, key)
-            if self._prepared:
-                self.__write("frontend prepared-queries 1")
-                assert self.__line()[0] == "ok", """\
-enabling prepared queries mode failed"""
+            self._configuration_hook()
 
-            self.__write("")
-            assert self.__line()[0] == "ok", """\
+            self._write("")
+            assert self._line()[0] == "ok", """\
 transitioning to training mode failed"""
         return self._program
 
-    def __init__(self, args, encoder, params, prepared = False):
+    def __init__(self, args, encoder, params):
         self.name = "Subprocess(program = %s, %s)" % \
             (basename(args[0]), str(params))
         self._program = None
         self._args = args
         self._encoder = encoder
         self._params = params
-        self._prepared = prepared
-        if not prepared:
-            self.query = self.__query_normal
-        else:
-            self.query = self.__query_prepared
-        self._result_count = None
 
     def get_memory_usage(self):
         if not self._program:
-            self.__get_program_handle()
+            self._get_program_handle()
         return psutil.Process(pid=self._program.pid).memory_info().rss / 1024
-
-    def supports_prepared_queries(self):
-        return self._prepared
 
     def fit(self, X):
         for entry in X:
-            d = Subprocess.__quote(self._encoder(entry))
-            self.__write(d)
-            assert self.__line()[0] == "ok", """\
+            d = Subprocess._quote(self._encoder(entry))
+            self._write(d)
+            assert self._line()[0] == "ok", """\
 encoded training point '%s' was rejected""" % d
-        self.__write("")
-        assert self.__line()[0] == "ok", """\
+        self._write("")
+        assert self._line()[0] == "ok", """\
 transitioning to query mode failed"""
 
-    def __query_normal(self, v, n):
-        d = Subprocess.__quote(self._encoder(v))
-        self.__write("%s %d" % (d, n))
-        return self.__handle_query_response()
+    def query(self, v, n):
+        d = Subprocess._quote(self._encoder(v))
+        self._write("%s %d" % (d, n))
+        return self._handle_query_response()
 
-    def __query_prepared(self, v, n):
-        self.prepare_query(v, n)
-        return self.run_prepared_query()
-
-    def __handle_query_response(self):
-        status = self.__line()
+    def _handle_query_response(self):
+        status = self._line()
         if status[0] == "ok":
             count = int(status[1])
-            results = []
-            i = 0
-            while i < count:
-                line = self.__line()
-                results.append(int(line[0]))
-                i += 1
-            assert len(results) == count
-            return results
+            return self._collect_query_response_lines(count)
         else:
             assert status[0] == "fail", """\
 query neither succeeded nor failed"""
             return []
 
-    def prepare_query(self, v, n):
-        d = Subprocess.__quote(self._encoder(v))
-        self.__write("%s %d" % (d, n))
-        assert self.__line()[0] == "ok", """\
-preparing the query '%s' failed""" % d
-
-    def run_prepared_query(self):
-        self.__write("query")
-        status = self.__line()
-        if status[0] == "ok":
-            self._result_count = int(status[1])
-        else:
-            assert status[0] == "fail", """\
-query neither succeeded nor failed"""
-            self._result_count = 0
-
-    def get_prepared_query_results(self):
-        results = []
-        i = 0
-        while i < self._result_count:
-            line = self.__line()
-            results.append(int(line[0]))
-            i += 1
-        self._result_count = 0
-        return results
+    def _collect_query_response_lines(self, count):
+            results = []
+            i = 0
+            while i < count:
+                line = self._line()
+                results.append(int(line[0]))
+                i += 1
+            return results
 
     def done(self):
         if self._program:
@@ -148,8 +114,44 @@ query neither succeeded nor failed"""
                 self._program.terminate()
 
 class PreparedSubprocess(Subprocess):
-    def __init__(self, args, encoder, params, prepared = False):
-        super.__init__(args, encoder, params, True)
+    def __init__(self, args, encoder, params):
+        super(PreparedSubprocess, self).__init__(args, encoder, params)
+        self._result_count = None
+
+    def _configuration_hook(self):
+        self._write("frontend prepared-queries 1")
+        assert self._line()[0] == "ok", """\
+enabling prepared queries mode failed"""
+
+    def query(self, v, n):
+        self.prepare_query(v, n)
+        self.run_prepared_query()
+        return self.get_prepared_query_results()
+
+    def prepare_query(self, v, n):
+        d = Subprocess._quote(self._encoder(v))
+        self._write("%s %d" % (d, n))
+        assert self._line()[0] == "ok", """\
+preparing the query '%s' failed""" % d
+
+    def run_prepared_query(self):
+        self._write("query")
+        status = self._line()
+        if status[0] == "ok":
+            self._result_count = int(status[1])
+        else:
+            assert status[0] == "fail", """\
+query neither succeeded nor failed"""
+            self._result_count = 0
+
+    def get_prepared_query_results(self):
+        if self._result_count:
+            try:
+                return self._collect_query_response_lines(self._result_count)
+            finally:
+                self._result_count = 0
+        else:
+            return []
 
 def BitSubprocess(args, params):
     return Subprocess(args, bit_unparse_entry, params)
