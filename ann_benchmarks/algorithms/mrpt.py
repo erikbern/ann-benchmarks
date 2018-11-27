3e6ee75250b7338e2d6fb102a9cd85e43613fd52
Ville Hyvonen <ville.o.hyvonen@helsinki.fi>
2018-11-27 13:43:09 +0000

    Change mrpt to use autotuning and update mrpt parameters

--- ann_benchmarks/algorithms/mrpt.py
+++ ann_benchmarks/algorithms/mrpt.py
@@ -5,27 +5,26 @@ import mrpt
 from ann_benchmarks.algorithms.base import BaseANN
 
 class MRPT(BaseANN):
-    def __init__(self, metric, n_trees, depth):
+    def __init__(self, metric, count):
         self._metric = metric
-        self._n_trees = n_trees
-        self._depth = depth
-        self._votes_required = None
+        self._k = count
 
     def fit(self, X):
         if self._metric == 'angular':
             X = sklearn.preprocessing.normalize(X, axis=1, norm='l2')
 
-        self._index = mrpt.MRPTIndex(X, depth=self._depth, n_trees=self._n_trees)
-        self._index.build()
+        self._index_autotuned = mrpt.MRPTIndex(X)
+        self._index_autotuned.build_autotune_sample(target_recall = None, k = self._k, n_test = 1000)
 
-    def set_query_arguments(self, votes_required):
-        self._votes_required = votes_required
+    def set_query_arguments(self, target_recall):
+        self._target_recall = target_recall
+        self._index = self._index_autotuned.subset(target_recall)
 
     def query(self, v, n):
         if self._metric == 'angular':
             v /= numpy.linalg.norm(v)
 
-        return self._index.ann(v, n, votes_required=self._votes_required)
+        return self._index.ann(v)
 
     def __str__(self):
-        return 'MRPT(n_trees=%d, depth=%d, votes_required=%d)' % (self._n_trees, self._depth, self._votes_required)
+        return 'MRPT(target recall = %.3f)' % (self._target_recall)
