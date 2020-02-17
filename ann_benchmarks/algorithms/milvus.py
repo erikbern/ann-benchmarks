8aa756a778023049028ca5b69f3f28d66157a46d
Erik Bernhardsson <mail@erikbern.com>
2020-02-16 21:49:28 -0500

    make it actually work

---
+++ ann_benchmarks/algorithms/milvus.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+import milvus
+import numpy
+import sklearn.preprocessing
+from ann_benchmarks.algorithms.base import BaseANN
+
+
+class Milvus(BaseANN):
+    def __init__(self, metric, nlist):
+        self._nlist = nlist
+        self._nprobe = None
+        self._metric = metric
+        self._milvus = milvus.Milvus()
+        self._milvus.connect(host='localhost', port='19530')
+        self._table_name = 'test01'
+
+    def fit(self, X):
+        if self._metric == 'angular':
+            X = sklearn.preprocessing.normalize(X, axis=1, norm='l2')
+
+        self._milvus.create_table({'table_name': self._table_name, 'dimension': X.shape[1]})
+        status, ids = self._milvus.insert(table_name=self._table_name, records=X.tolist())
+        self._milvus.create_index(self._table_name, {'index_type': milvus.IndexType.FLAT, 'nlist': self._nlist})
+        self._milvus_id_to_index = {}
+        for i, id in enumerate(ids):
+            self._milvus_id_to_index[id] = i
+
+    def set_query_arguments(self, nprobe):
+        self._nprobe = nprobe
+
+    def query(self, v, n):
+        if self._metric == 'angular':
+            v /= numpy.linalg.norm(v)
+        v = v.tolist()
+        status, results = self._milvus.search(table_name=self._table_name, query_records=[v], top_k=n, nprobe=self._nprobe)
+        if not results:
+            return []  # Seems to happen occasionally, not sure why
+        r = [self._milvus_id_to_index[z.id] for z in results[0]]
+        return r
+
+    def __str__(self):
+        return 'Milvus(nlist=%d, nprobe=%d)' % (self._nlist, self._nprobe)
