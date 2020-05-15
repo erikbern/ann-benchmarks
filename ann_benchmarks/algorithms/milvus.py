4f84a14bedfca4745f8ba8dfa9b1a4f9f4cf1fa2
yhz <413554850@qq.com>
2020-05-15 11:31:52 +0800

    update ann for 090
    Signed-off-by: yhz <413554850@qq.com>

--- ann_benchmarks/algorithms/milvus.py
+++ ann_benchmarks/algorithms/milvus.py
@@ -7,11 +7,10 @@ from ann_benchmarks.algorithms.base import BaseANN
 
 class Milvus(BaseANN):
     def __init__(self, metric, index_type, nlist):
-        self._nlist = nlist
-        self._nprobe = None
+        self._index_param = {'nlist': nlist}
+        self._search_param = {'nprobe': None}
         self._metric = metric
-        self._milvus = milvus.Milvus()
-        self._milvus.connect(host='localhost', port='19530')
+        self._milvus = milvus.Milvus(host='localhost', port='19530')
         self._table_name = 'test01'
         self._index_type = index_type
 
@@ -19,27 +18,29 @@ class Milvus(BaseANN):
         if self._metric == 'angular':
             X = sklearn.preprocessing.normalize(X, axis=1, norm='l2')
 
-        self._milvus.create_table({'table_name': self._table_name, 'dimension': X.shape[1]})
+        self._milvus.create_collection({'collection_name': self._table_name, 'dimension': X.shape[1]})
         vector_ids = [id for id in range(len(X))]
-        status, ids = self._milvus.insert(table_name=self._table_name, records=X.tolist(), ids=vector_ids)
+        status, ids = self._milvus.insert(collection_name=self._table_name, records=X.tolist(), ids=vector_ids)
         index_type = getattr(milvus.IndexType, self._index_type)  # a bit hacky but works
-        self._milvus.create_index(self._table_name, {'index_type': index_type, 'nlist': self._nlist})
+        self._milvus.create_index(self._table_name, index_type, params=self._index_param)
 #         self._milvus_id_to_index = {}
 #         self._milvus_id_to_index[-1] = -1 #  -1 means no results found
 #         for i, id in enumerate(ids):
 #             self._milvus_id_to_index[id] = i
 
     def set_query_arguments(self, nprobe):
-        if nprobe > self._nlist:
+        if nprobe > self._index_param['nlist']:
             print('warning! nprobe > nlist')
-            nprobe = self._nlist
-        self._nprobe = nprobe
+            nprobe = self._index_param['nlist']
+        self._search_param['nprobe'] = nprobe
 
     def query(self, v, n):
         if self._metric == 'angular':
             v /= numpy.linalg.norm(v)
         v = v.tolist()
-        status, results = self._milvus.search(table_name=self._table_name, query_records=[v], top_k=n, nprobe=self._nprobe)
+        future = self._milvus.search(collection_name=self._table_name, query_records=[v], top_k=n, params=self._search_param, _async=True)
+        status, results = future.result()
+
         if not results:
             return []  # Seems to happen occasionally, not sure why
         #r = [self._milvus_id_to_index[z.id] for z in results[0]]
@@ -49,4 +50,4 @@ class Milvus(BaseANN):
         return results_ids
 
     def __str__(self):
-        return 'Milvus(index_type=%s, nlist=%d, nprobe=%d)' % (self._index_type, self._nlist, self._nprobe)
+        return 'Milvus(index={}, index_param={}, search_param={})'.format(self._index_type, self._index_param, self._search_param)
