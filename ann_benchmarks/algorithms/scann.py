dc90f577f3ce10c30341f890b7bf71e0b34035ca
Philip Sun <sunphil@google.com>
2021-01-14 16:20:16 +0000

    Update ScaNN to support Euclidean datasets

--- ann_benchmarks/algorithms/scann.py
+++ ann_benchmarks/algorithms/scann.py
@@ -6,19 +6,24 @@ from ann_benchmarks.algorithms.base import BaseANN
 
 class Scann(BaseANN):
 
-  def __init__(self, n_leaves, avq_threshold, dims_per_block):
+  def __init__(self, n_leaves, avq_threshold, dims_per_block, dist):
     self.name = "scann n_leaves={} avq_threshold={:.02f} dims_per_block={}".format(
             n_leaves, avq_threshold, dims_per_block)
     self.n_leaves = n_leaves
     self.avq_threshold = avq_threshold
     self.dims_per_block = dims_per_block
+    self.dist = dist
 
   def fit(self, X):
-    X[np.linalg.norm(X, axis=1) == 0] = 1.0 / np.sqrt(X.shape[1])
-    X /= np.linalg.norm(X, axis=1)[:, np.newaxis]
+    if self.dist == "dot_product":
+      spherical = True
+      X[np.linalg.norm(X, axis=1) == 0] = 1.0 / np.sqrt(X.shape[1])
+      X /= np.linalg.norm(X, axis=1)[:, np.newaxis]
+    else:
+      spherical = False
 
-    self.searcher = scann.scann_ops_pybind.builder(X, 10, "dot_product").tree(
-        self.n_leaves, 1, training_sample_size=350000, spherical=True, quantize_centroids=True).score_ah(
+    self.searcher = scann.scann_ops_pybind.builder(X, 10, self.dist).tree(
+        self.n_leaves, 1, training_sample_size=len(X), spherical=spherical, quantize_centroids=True).score_ah(
             self.dims_per_block, anisotropic_quantization_threshold=self.avq_threshold).reorder(
                 1).build()
 
