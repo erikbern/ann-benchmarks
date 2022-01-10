a5638a6fa226f84e5e2c2ae5443cc24e50e29183
Martin Aumueller <maau@itu.dk>
2022-01-10 13:49:55 +0100

    pandas -> csv

--- data_export.py
+++ data_export.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import argparse
+import csv
 
 from ann_benchmarks.datasets import DATASETS, get_dataset
 from ann_benchmarks.plotting.utils  import compute_metrics_all_runs
@@ -14,20 +14,25 @@ if __name__ == "__main__":
     parser.add_argument(
         '--recompute',
         action='store_true',
-        help='Path to the output csv file')
+        help='Recompute metrics')
     args = parser.parse_args()
 
     datasets = DATASETS.keys()
     dfs = []
-
     for dataset_name in datasets:
         print("Looking at dataset", dataset_name)
         if len(list(load_all_results(dataset_name))) > 0:
             results = load_all_results(dataset_name)
             dataset, _ = get_dataset(dataset_name)
             results = compute_metrics_all_runs(dataset, results, args.recompute)
-            dfs.append(pd.DataFrame(results))
+            for res in results:
+                res['dataset'] = dataset_name
+                dfs.append(res)
     if len(dfs) > 0:
-        data = pd.concat(dfs)
-        data.to_csv(args.output, index=False)
+        with open(args.output, 'w', newline='') as csvfile:
+            names = list(dfs[0].keys())
+            writer = csv.DictWriter(csvfile, fieldnames=names)
+            writer.writeheader()
+            for res in dfs:
+                writer.writerow(res)
 
