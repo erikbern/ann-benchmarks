8e7a016c0155d7d692f8a2f86239af55d0bc45da
GuyAv46 <guy.avimor@gmail.com>
2022-02-03 16:46:27 +0200

    update redisearch API and capabilities

--- ann_benchmarks/algorithms/redisearch.py
+++ ann_benchmarks/algorithms/redisearch.py
@@ -1,40 +1,39 @@
 from __future__ import absolute_import
-import os
-import base64
 from redis import Redis
-from redisearch import Client, Query 
-import numpy as np
+from redisearch import Client, Query
 from ann_benchmarks.constants import INDEX_DIR
 from ann_benchmarks.algorithms.base import BaseANN
 
 
 class RediSearch(BaseANN):
-    def __init__(self, metric, method_param):
+    def __init__(self, algo, metric, conn_params, method_param):
         self.metric = {'angular': 'cosine', 'euclidean': 'l2'}[metric]
         self.method_param = method_param
+        self.algo = algo
         # print(self.method_param,save_index,query_param)
         # self.ef=query_param['ef']
-        self.name = 'redisearch (%s)' % (self.method_param)
+        self.name = 'redisearch-%s (%s)' % (self.algo, self.method_param)
         self.index_name = "ann_benchmark"
-        self.client = Client(self.index_name, conn = Redis(decode_responses=False))
+        conn = Redis(host=conn_params["host"], port=conn_params["port"],
+                     password=conn_params["auth"], username=conn_params["user"],
+                     decode_responses=False)
+        self.client = Client(self.index_name, conn = conn)
 
-    def fit(self, X):
-        # Only l2 is supported currently
-        self.client.redis.execute_command('FT.CREATE', self.index_name, 'SCHEMA', 'vector',  'VECTOR', 'FLOAT32', len(X[0]), 'L2', 'HNSW', 'INITIAL_CAP', len(X), 'M', self.method_param['M'] , 'EF', self.method_param["efConstruction"])
+    def fit(self, X, offset=0, cap=None):
+        try:
+            self.client.redis.execute_command('FT.CREATE', self.index_name, 'SCHEMA', 'vector', 'VECTOR', self.algo, '12', 'TYPE', 'FLOAT32', 'DIM', len(X[0]), 'DISTANCE_METRIC', self.metric, 'INITIAL_CAP', cap if cap else len(X), 'M', self.method_param['M'] , 'EF_CONSTRUCTION', self.method_param["efConstruction"])
+        except Exception:
+            pass
+
         for i, x in enumerate(X):
-            self.client.redis.execute_command('HSET', f'ann_{i}', 'vector', x.tobytes())
-
+            self.client.redis.execute_command('HSET', f'ann_{i+offset}', 'vector', x.tobytes())
 
     def set_query_arguments(self, ef):
         self.ef = ef
 
     def query(self, v, k):
-        base64_vector = base64.b64encode(v).decode('ascii')
-        base64_vector_escaped = base64_vector.translate(str.maketrans({"=":  r"\=",
-                                              "/":  r"\/",
-                                              "+":  r"\+"}))
-        q = Query('@vector:[' + base64_vector_escaped + ' TOPK ' +str(k)+'] => {$BASE64:TRUE; $efruntime:' + str(self.ef) + '}').sort_by('vector', asc=True).no_content()
-        return [int(doc.id.replace('ann_','')) for doc in self.client.search(q).docs]
+        q = Query('*=>[TOP_K ' + str(k) + ' @vector $BLOB EF_RUNTIME ' + str(self.ef) + ']').sort_by('__vector_score', asc=True).no_content()
+        return [int(doc.id.replace('ann_','')) for doc in self.client.search(q, query_params = {'BLOB': v.tobytes()}).docs]
 
     def freeIndex(self):
         self.client.redis.execute_command("FLUSHALL")
