418bb2f4b4fc87bd3468bd9d902a37c10cea4cd1
GuyAv46 <guy.avimor@gmail.com>
2022-02-06 18:26:08 +0200

    some review modifications

--- ann_benchmarks/algorithms/redisearch.py
+++ ann_benchmarks/algorithms/redisearch.py
@@ -10,8 +10,6 @@ class RediSearch(BaseANN):
         self.metric = {'angular': 'cosine', 'euclidean': 'l2'}[metric]
         self.method_param = method_param
         self.algo = algo
-        # print(self.method_param,save_index,query_param)
-        # self.ef=query_param['ef']
         self.name = 'redisearch-%s (%s)' % (self.algo, self.method_param)
         self.index_name = "ann_benchmark"
         conn = Redis(host=conn_params["host"], port=conn_params["port"],
@@ -19,20 +17,29 @@ class RediSearch(BaseANN):
                      decode_responses=False)
         self.client = Client(self.index_name, conn = conn)
 
-    def fit(self, X, offset=0, cap=None):
+    def fit(self, X, offset=0, limit=None):
+        limit = limit if limit else len(X)
+        # print('inserting %d out of %d vectors' % (limit-offset, len(X)))
         try:
-            self.client.redis.execute_command('FT.CREATE', self.index_name, 'SCHEMA', 'vector', 'VECTOR', self.algo, '12', 'TYPE', 'FLOAT32', 'DIM', len(X[0]), 'DISTANCE_METRIC', self.metric, 'INITIAL_CAP', cap if cap else len(X), 'M', self.method_param['M'] , 'EF_CONSTRUCTION', self.method_param["efConstruction"])
-        except Exception:
-            pass
+            if self.algo == "HNSW":
+                self.client.redis.execute_command('FT.CREATE', self.index_name, 'SCHEMA', 'vector', 'VECTOR', self.algo, '12', 'TYPE', 'FLOAT32', 'DIM', len(X[0]), 'DISTANCE_METRIC', self.metric, 'INITIAL_CAP', len(X), 'M', self.method_param['M'] , 'EF_CONSTRUCTION', self.method_param["efConstruction"])
+            elif self.algo == "FLAT":
+                self.client.redis.execute_command('FT.CREATE', self.index_name, 'SCHEMA', 'vector', 'VECTOR', self.algo, '10', 'TYPE', 'FLOAT32', 'DIM', len(X[0]), 'DISTANCE_METRIC', self.metric, 'INITIAL_CAP', len(X), 'BLOCK_SIZE', self.method_param['BLOCK_SIZE'])
 
-        for i, x in enumerate(X):
-            self.client.redis.execute_command('HSET', f'ann_{i+offset}', 'vector', x.tobytes())
+        except Exception as e:
+            if 'Index already exists' not in str(e):
+                raise
+
+        for i in range(offset, limit):
+            self.client.redis.execute_command('HSET', f'ann_{i}', 'vector', X[i].tobytes())
 
     def set_query_arguments(self, ef):
         self.ef = ef
 
     def query(self, v, k):
-        q = Query('*=>[TOP_K ' + str(k) + ' @vector $BLOB EF_RUNTIME ' + str(self.ef) + ']').sort_by('__vector_score', asc=True).no_content()
+        vq = '*=>[TOP_K ' + str(k) + ' @vector $BLOB'
+        vq += ((' EF_RUNTIME ' + str(self.ef)) if self.algo == 'HNSW' else '')
+        q = Query(vq + ']').sort_by('__vector_score', asc=True).paging(0, k).no_content()
         return [int(doc.id.replace('ann_','')) for doc in self.client.search(q, query_params = {'BLOB': v.tobytes()}).docs]
 
     def freeIndex(self):
