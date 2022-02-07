fd92d61a7c696eeaa0eddae7421283f33d95d3d8
GuyAv46 <guy.avimor@gmail.com>
2022-02-07 19:04:34 +0200

    review changes

--- ann_benchmarks/algorithms/redisearch.py
+++ ann_benchmarks/algorithms/redisearch.py
@@ -20,8 +20,8 @@ class RediSearch(BaseANN):
 
     def fit(self, X, offset=0, limit=None):
         limit = limit if limit else len(X)
-        # print('inserting %d out of %d vectors' % (limit-offset, len(X)))
         try:
+            # https://oss.redis.com/redisearch/master/Commands/#ftcreate
             if self.algo == "HNSW":
                 self.redis.execute_command('FT.CREATE', self.index_name, 'SCHEMA', 'vector', 'VECTOR', self.algo, '12', 'TYPE', 'FLOAT32', 'DIM', len(X[0]), 'DISTANCE_METRIC', self.metric, 'INITIAL_CAP', len(X), 'M', self.method_param['M'] , 'EF_CONSTRUCTION', self.method_param["efConstruction"], target_nodes='random')
             elif self.algo == "FLAT":
@@ -37,8 +37,9 @@ class RediSearch(BaseANN):
         self.ef = ef
 
     def query(self, v, k):
-        qparams = (' EF_RUNTIME ' + str(self.ef)) if self.algo == 'HNSW' else ''
-        vq = '*=>[TOP_K ' + str(k) + ' @vector $BLOB' + qparams + ']'
+        # https://oss.redis.com/redisearch/master/Commands/#ftsearch
+        qparams = f' EF_RUNTIME {self.ef}' if self.algo == 'HNSW' else ''
+        vq = f'*=>[TOP_K {k} @vector $BLOB {qparams}]'
         q = ['FT.SEARCH', self.index_name, vq, 'NOCONTENT', 'SORTBY', '__vector_score', 'LIMIT', '0', str(k), 'PARAMS', '2', 'BLOB', v.tobytes()]
         return [int(doc.replace(b'ann_',b'')) for doc in self.redis.execute_command(*q, target_nodes='random')[1:]]
 
