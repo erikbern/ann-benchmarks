38b61a20992be4272e4ac2c489ac2548bf1c0c55
DvirDukhan <dvir@redis.com>
2022-03-09 09:10:28 +0200

    hybrid dataset generator. Redisearch hybrid load and run

--- ann_benchmarks/algorithms/redisearch.py
+++ ann_benchmarks/algorithms/redisearch.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from optparse import Values
 from redis import Redis
 from redis.cluster import RedisCluster
 from ann_benchmarks.constants import INDEX_DIR
@@ -19,28 +20,62 @@ class RediSearch(BaseANN):
         self.redis = redis(host=host, port=port, decode_responses=False,
                            password=conn_params["auth"], username=conn_params["user"])
 
-    def fit(self, X, offset=0, limit=None):
+    def fit(self, X, offset=0, limit=None, hybrid_buckets = None):
         limit = limit if limit else len(X)
         try:
+            args = [self.index_name, 'SCHEMA']
+            if hybrid_buckets:
+                args.extend(['n', 'NUMERIC', 't', 'TEXT'])
             # https://oss.redis.com/redisearch/master/Commands/#ftcreate
             if self.algo == "HNSW":
-                self.redis.execute_command('FT.CREATE', self.index_name, 'SCHEMA', 'vector', 'VECTOR', self.algo, '12', 'TYPE', 'FLOAT32', 'DIM', len(X[0]), 'DISTANCE_METRIC', self.metric, 'INITIAL_CAP', len(X), 'M', self.method_param['M'], 'EF_CONSTRUCTION', self.method_param["efConstruction"], target_nodes='random')
+                args.extend(['vector', 'VECTOR', self.algo, '12', 'TYPE', 'FLOAT32', 'DIM', len(X[0]), 'DISTANCE_METRIC', self.metric, 'INITIAL_CAP', len(X), 'M', self.method_param['M'], 'EF_CONSTRUCTION', self.method_param["efConstruction"]])
             elif self.algo == "FLAT":
-                self.redis.execute_command('FT.CREATE', self.index_name, 'SCHEMA', 'vector', 'VECTOR', self.algo, '10', 'TYPE', 'FLOAT32', 'DIM', len(X[0]), 'DISTANCE_METRIC', self.metric, 'INITIAL_CAP', len(X), 'BLOCK_SIZE', self.method_param['BLOCK_SIZE'], target_nodes='random')
+                args.extend(['vector', 'VECTOR', self.algo, '10', 'TYPE', 'FLOAT32', 'DIM', len(X[0]), 'DISTANCE_METRIC', self.metric, 'INITIAL_CAP', len(X), 'BLOCK_SIZE', self.method_param['BLOCK_SIZE']])
+            self.redis.execute_command('FT.CREATE', *args,  target_nodes='random')
         except Exception as e:
             if 'Index already exists' not in str(e):
                 raise
-
-        for i in range(offset, limit):
-            self.redis.execute_command('HSET', f'ann_{i}', 'vector', X[i].tobytes())
+        p = self.redis.pipeline(transaction=False)
+        count = 0
+        if hybrid_buckets:
+            print('running hybrid')
+            for bucket in hybrid_buckets.values():
+                ids = bucket['ids']
+                text = bucket['text'].decode()
+                number = bucket['number']
+                print('calling HSET', f'ann_<id>', 'vector', '<vector blob>', 't', text, 'n', number)
+                for id in ids:
+                    if id >= offset and id < limit:
+                        p.execute_command('HSET', f'ann_{id}', 'vector', X[id].tobytes(), 't', text, 'n', int(number))
+                        count+=1
+                        if count == 1000:
+                            p.execute()
+                            p.reset()
+                            count = 0
+            p.execute()
+        else:
+            for i in range(offset, limit):
+                p.execute_command('HSET', f'ann_{i}', 'vector', X[i].tobytes())
+                count+=1
+                if count == 1000:
+                    p.execute()
+                    p.reset()
+                    count = 0
+            p.execute()
 
     def set_query_arguments(self, ef):
         self.ef = ef
 
+    def set_hybrid_query(self, text):
+        self.text = text
+
     def query(self, v, k):
         # https://oss.redis.com/redisearch/master/Commands/#ftsearch
         qparams = f' EF_RUNTIME {self.ef}' if self.algo == 'HNSW' else ''
-        vq = f'*=>[KNN {k} @vector $BLOB {qparams}]'
+        if self.text:
+            vq = f'(@t:{self.text})=>[KNN {k} @vector $BLOB {qparams}]'
+        else:
+            vq = f'*=>[KNN {k} @vector $BLOB {qparams}]'
         q = ['FT.SEARCH', self.index_name, vq, 'NOCONTENT', 'SORTBY', '__vector_score', 'LIMIT', '0', str(k), 'PARAMS', '2', 'BLOB', v.tobytes()]
         return [int(doc.replace(b'ann_',b'')) for doc in self.redis.execute_command(*q, target_nodes='random')[1:]]
 
