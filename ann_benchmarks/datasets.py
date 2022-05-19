ec0537bff70293702ffd1b40ee145b4bb5673d85
DvirDukhan <dvir@redislabs.com>
2022-05-19 12:28:53 +0000

    fixed amazon review dataset creation

--- ann_benchmarks/datasets.py
+++ ann_benchmarks/datasets.py
@@ -540,16 +540,24 @@ def amazon_reviews(out_fn):
     import os
     import math
     import pickle
+    import numpy as np
     subsets = ['Wireless_v1_00', 'Watches_v1_00', 'Video_Games_v1_00', 'Video_DVD_v1_00', 'Video_v1_00', 'Toys_v1_00', 'Tools_v1_00', 'Sports_v1_00', 'Software_v1_00', 'Shoes_v1_00', 'Pet_Products_v1_00', 'Personal_Care_Appliances_v1_00', 'PC_v1_00', 'Outdoors_v1_00', 'Office_Products_v1_00', 'Musical_Instruments_v1_00', 'Music_v1_00', 'Mobile_Electronics_v1_00', 'Mobile_Apps_v1_00', 'Major_Appliances_v1_00', 'Luggage_v1_00', 'Lawn_and_Garden_v1_00', 'Kitchen_v1_00', 'Jewelry_v1_00', 'Home_Improvement_v1_00', 'Home_Entertainment_v1_00', 'Home_v1_00', 'Health_Personal_Care_v1_00', 'Grocery_v1_00', 'Gift_Card_v1_00', 'Furniture_v1_00', 'Electronics_v1_00', 'Digital_Video_Games_v1_00', 'Digital_Video_Download_v1_00', 'Digital_Software_v1_00', 'Digital_Music_Purchase_v1_00', 'Digital_Ebook_Purchase_v1_00', 'Camera_v1_00', 'Books_v1_00', 'Beauty_v1_00', 'Baby_v1_00', 'Automotive_v1_00', 'Apparel_v1_00', 'Digital_Ebook_Purchase_v1_01', 'Books_v1_01', 'Books_v1_02']
-    train_set = []
-    test_set = []
-    for subset in subsets:
+    train_set = None
+    test_set = None
+    for i, subset in enumerate(subsets):
         url = f'https://s3.us-east-1.amazonaws.com/benchmarks.redislabs/vecsim/amazon_reviews/{subset}_embeddings'
         local_fn = f'{subset}_embeddings'
         download(url, local_fn)
         subset_embeddings = pickle.load(open(local_fn, "rb"))
-        train_set.extend(subset_embeddings)
-        test_set.extend(subset_embeddings[:math.ceil(10000/len(subsets))])
+        if i==0:
+            train_set = subset_embeddings
+            test_set = subset_embeddings[:math.ceil(10000/len(subsets))]
+        else:
+            train_set = np.append(train_set, subset_embeddings, axis =0)
+            test_set = np.append(test_set, subset_embeddings[:math.ceil(10000/len(subsets))], axis=0)
+        print(subset_embeddings.shape)
+        print(train_set.shape)
+        print(test_set.shape)
         os.remove(local_fn)
     write_output(train_set, test_set[:10000], out_fn, 'angular')
 
