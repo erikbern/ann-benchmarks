cc576ab5e4ca24501cdd5db27a9da1a1f118237a
wangting0128 <ting.wang@zilliz.com>
2023-02-10 18:18:28 +0800

    update milvus's version

--- ann_benchmarks/algorithms/milvus.py
+++ ann_benchmarks/algorithms/milvus.py
@@ -1,45 +1,48 @@
 from __future__ import absolute_import
-import milvus
 import numpy
+import pyknowhere
 import sklearn.preprocessing
 from ann_benchmarks.algorithms.base import BaseANN
 
 
+def metric_mapping(_metric: str):
+    _metric_type = {'angular': 'cosine', 'euclidean': 'l2'}.get(_metric, None)
+    if _metric_type is None:
+        raise Exception(f"[Milvus] Not support metric type: {_metric}!!!")
+    return _metric_type
+
+
 class Milvus(BaseANN):
-    def __init__(self, metric, index_type, nlist):
-        self._nlist = nlist
-        self._nprobe = None
+    """
+    Needs `__AVX512F__` flag to run, otherwise the results are incorrect.
+    Support HNSW index type
+    """
+    def __init__(self, metric, dim, index_param):
         self._metric = metric
-        self._milvus = milvus.Milvus()
-        self._milvus.connect(host='localhost', port='19530')
-        self._table_name = 'test01'
-        self._index_type = index_type
+        self._dim = dim
+        self._metric_type = metric_mapping(self._metric)
+        self._index_m = index_param.get("M", None)
+        self._index_ef = index_param.get("efConstruction", None)
+        self._search_ef = None
+
+        self.client = None
 
     def fit(self, X):
         if self._metric == 'angular':
             X = sklearn.preprocessing.normalize(X, axis=1, norm='l2')
 
-        self._milvus.create_table({'table_name': self._table_name, 'dimension': X.shape[1]})
-        vector_ids = [id for id in range(len(X))]
-        self._milvus.insert(table_name=self._table_name, records=X.tolist(), ids=vector_ids)
-        index_type = getattr(milvus.IndexType, self._index_type)  # a bit hacky but works
-        self._milvus.create_index(self._table_name, {'index_type': index_type, 'nlist': self._nlist})
+        self.client = pyknowhere.Index(self._metric_type, self._dim, len(X), self._index_m, self._index_ef)
+        self.client.add(X, numpy.arange(len(X)))
 
-    def set_query_arguments(self, nprobe):
-        if nprobe > self._nlist:
-            print('warning! nprobe > nlist')
-            nprobe = self._nlist
-        self._nprobe = nprobe
+    def set_query_arguments(self, ef):
+        self._search_ef = ef
+        self.client.set_param(ef)
 
     def query(self, v, n):
         if self._metric == 'angular':
             v /= numpy.linalg.norm(v)
-        v = v.tolist()
-        status, results = self._milvus.search(table_name=self._table_name, query_records=[v], top_k=n, nprobe=self._nprobe)
-        if not results:
-            return []  # Seems to happen occasionally, not sure why
-        result_ids = [result.id for result in results[0]]
-        return result_ids
+
+        return self.client.search(v, k=n)
 
     def __str__(self):
-        return 'Milvus(index_type=%s, nlist=%d, nprobe=%d)' % (self._index_type, self._nlist, self._nprobe)
+        return f"Milvus(Knowhere)(index_M:{self._index_m},index_ef:{self._index_ef},search_ef={self._search_ef})"
