de5a914c5ef41ccccbc9bcfef018a08c99858b0b
Corey J. Nolet <cjnolet@gmail.com>
2023-02-28 13:03:46 -0500

    Properly handle docker return status. not sure why this is different now?

--- ann_benchmarks/runner.py
+++ ann_benchmarks/runner.py
@@ -288,12 +288,15 @@ def run_docker(definition, dataset, count, runs, timeout, batch, cpu_limit,
     finally:
         container.remove(force=True)
 
+
 def _handle_container_return_value(return_value, container, logger):
     base_msg = 'Child process for container %s' % (container.short_id)
-    if type(return_value) is dict: # The return value from container.wait changes from int to dict in docker 3.0.0
-        error_msg = return_value['Error']
+    # The return value from container.wait changes from int to dict in docker 3.0.0
+    if type(return_value) is dict:
         exit_code = return_value['StatusCode']
-        msg = base_msg + 'returned exit code %d with message %s' %(exit_code, error_msg)
+        if 'Error' in return_value:
+            error_msg = 'Error' in return_value['Error']
+            msg = base_msg + 'returned exit code %d with message %s' %(exit_code, error_msg)
     else: 
         exit_code = return_value
         msg = base_msg + 'returned exit code %d' % (exit_code)
