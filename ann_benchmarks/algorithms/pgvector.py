42e53c2e7ffe524fd304ab1e919f23c6e29e0556
Erik Bernhardsson <mail@erikbern.com>
2023-04-07 09:12:12 -0400

    Support querying

--- ann_benchmarks/algorithms/pgvector.py
+++ ann_benchmarks/algorithms/pgvector.py
@@ -1,8 +1,8 @@
 import subprocess
 import sys
 
-import psycopg2
-import pgvector.psycopg2
+import psycopg
+import pgvector.psycopg
 
 from ann_benchmarks.algorithms.base import BaseANN
 
@@ -10,13 +10,30 @@ class PGVector(BaseANN):
     def __init__(self, metric, lists):
         self._metric = metric
         self._lists = lists
+        self._cur = None
 
     def fit(self, X):
         subprocess.run("service postgresql start", shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
-        conn = psycopg2.connect(host="localhost", user="ann", password="ann", dbname="ann")
+        conn = psycopg.connect(host="localhost", user="ann", password="ann", dbname="ann")
+        pgvector.psycopg.register_vector(conn)
         cur = conn.cursor()
-        pgvector.psycopg2.register_vector(conn)
-        cur.execute("create table items (item int, embedding vector(%s))", (X.shape[1],))
-        for i, embedding in enumerate(X):
-            cur.execute('INSERT INTO items (item, embedding) VALUES (%s, %s)', (i, embedding,))
-        cur.execute('CREATE INDEX ON items USING ivfflat (embedding vector_cosine_ops) WITH (lists = %s)', (self._lists,))
+        cur.execute("CREATE TABLE items (id int, embedding vector(%d))" % X.shape[1])
+        print("copying data...")
+        with cur.copy("COPY items (id, embedding) FROM STDIN") as copy:
+            for i, embedding in enumerate(X):
+                copy.write_row((i, embedding))
+        print("creating index...")
+        cur.execute('CREATE INDEX ON items USING ivfflat (embedding vector_cosine_ops) WITH (lists = %d)' % self._lists)
+        print("done!")
+        self._cur = cur
+
+    def query(self, v, n):
+        if self._metric == "angular":
+            q = "SELECT id FROM items ORDER BY embedding <=> %s LIMIT %s"
+        elif self._metric == "euclidean":
+            q = "SELECT id FROM items ORDER BY embedding <-> %s LIMIT %s"
+        else:
+            raise RuntimeError(f"unknown metric {self._metric}")
+
+        self._cur.execute(q, (v, n))
+        return [id for id, in self._cur.fetchall()]
