a25c51df5c0eacd3dc90103151095e43f56c171c
Erik Bernhardsson <mail@erikbern.com>
2023-04-07 10:31:22 -0400

    Fix Hamming distance

--- test/test-distance.py
+++ test/test-distance.py
@@ -1,10 +1,17 @@
 import unittest
 import numpy
 
-from ann_benchmarks.distance import euclidean
+from ann_benchmarks.distance import metrics
 
 class TestDistance(unittest.TestCase):
     def test_euclidean(self):
         p = numpy.array([0, 1, 0])
         q = numpy.array([2, 0, 0])
-        self.assertAlmostEqual(euclidean(p, q), 5 ** 0.5)
+        dist = metrics["euclidean"]["distance"]
+        self.assertAlmostEqual(dist(p, q), 5 ** 0.5)
+
+    def test_hamming(self):
+        p = numpy.array([1, 1, 0, 0], dtype=numpy.bool_)
+        q = numpy.array([1, 0, 0, 1], dtype=numpy.bool_)
+        dist = metrics["hamming"]["distance"]
+        self.assertAlmostEqual(dist(p, q), 2)
