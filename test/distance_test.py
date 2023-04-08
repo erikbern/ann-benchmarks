399c708544acf169904d67085977022bc31d3d0d
Erik Bernhardsson <mail@erikbern.com>
2023-04-08 07:30:41 -0400

    Fix angular distance

--- test/distance_test.py
+++ test/distance_test.py
@@ -5,18 +5,36 @@ from ann_benchmarks.distance import metrics
 
 
 def test_euclidean():
+    dist = metrics["euclidean"]["distance"]
+
     p = numpy.array([0, 1, 0])
     q = numpy.array([2, 0, 0])
-    dist = metrics["euclidean"]["distance"]
     assert dist(p, q) == pytest.approx(5**0.5)
 
 
 def test_hamming():
+    dist = metrics["hamming"]["distance"]
+
     p = numpy.array([1, 1, 0, 0], dtype=numpy.bool_)
     q = numpy.array([1, 0, 0, 1], dtype=numpy.bool_)
-    dist = metrics["hamming"]["distance"]
     assert dist(p, q) == pytest.approx(2)
 
     p = numpy.array([1, 1, 0, 0])
     q = numpy.array([1, 0, 0, 1])
+    assert dist(p, q) == pytest.approx(2)
+
+
+def test_angular():
+    dist = metrics["angular"]["distance"]
+
+    p = numpy.array([5, 0])
+    q = numpy.array([0, 3])
+    assert dist(p, q) == pytest.approx(2**0.5)
+
+    p = numpy.array([5, 0])
+    q = numpy.array([1, 0])
+    assert dist(p, q) == pytest.approx(0)
+
+    p = numpy.array([7, 0])
+    q = numpy.array([-8, 0])
     assert dist(p, q) == pytest.approx(2)
