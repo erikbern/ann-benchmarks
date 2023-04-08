a1ae69dd5db386f4abe64a371eb31bb79f6ca91c
Erik Bernhardsson <mail@erikbern.com>
2023-04-08 07:30:41 -0400

    Fix angular distance

--- ann_benchmarks/distance.py
+++ ann_benchmarks/distance.py
@@ -34,7 +34,7 @@ metrics = {
         "distance_valid": lambda a: True,
     },
     "angular": {
-        "distance": lambda a, b: euclidean(a, b) / (norm(a) * norm(b)),
+        "distance": lambda a, b: euclidean(a / norm(a), b / norm(b)),
         "distance_valid": lambda a: True,
     },
 }
