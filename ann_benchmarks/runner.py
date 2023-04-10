bf5e77e73a7e8eac700d5603b29b5800e08a9d5e
Andrew Kane <andrew@ankane.org>
2023-04-10 12:50:42 -0700

    Fix KeyError [skip ci]

--- ann_benchmarks/runner.py
+++ ann_benchmarks/runner.py
@@ -261,7 +261,7 @@ def run_docker(definition, dataset, count, runs, timeout, batch, cpu_limit, mem_
 def _handle_container_return_value(return_value, container, logger):
     base_msg = "Child process for container %s" % (container.short_id)
     if type(return_value) is dict:  # The return value from container.wait changes from int to dict in docker 3.0.0
-        error_msg = return_value["Error"]
+        error_msg = return_value.get("Error")
         exit_code = return_value["StatusCode"]
         msg = base_msg + "returned exit code %d with message %s" % (exit_code, error_msg)
     else:
