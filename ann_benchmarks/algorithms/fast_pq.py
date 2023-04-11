6e9ca6ead1ae3cf3a6d0b3b683f6362062b2f82f
tahle <ahle@fb.com>
2023-04-11 12:26:41 -0700

    Added FastPQ

---
+++ ann_benchmarks/algorithms/fast_pq.py
@@ -0,0 +1,25 @@
+import fast_pq
+
+from .base import BaseANN
+
+
+class FastPQ(BaseANN):
+    def __init__(self, metric, build_probes):
+        self._build_probes = build_probes
+        self._query_probes = None
+        self._metric = metric
+
+    def fit(self, X):
+        n = X.shape[0]
+        self._ivf = IVF(self._metric, cl=int(n**.5 + 1), pq=fast_pq.FastPQ(dims_per_block=2))
+        self._ivf.fit(X).build(X, n_probes=self._build_probes)
+
+    def set_query_arguments(self, query_probes):
+        self._query_probes = query_probes
+
+    def query(self, v, n):
+        return self._ivf.query(v, k=n, n_probes=self._query_probes)
+
+    def __str__(self):
+        return f"FastPQ(build_probes={self._build_probes}, query_probes={self._query_probes})"
+
