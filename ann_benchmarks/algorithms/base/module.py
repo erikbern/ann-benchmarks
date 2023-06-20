a21773057e14975cbc66c411a627b44e03cccf0d
Jack Eadie <jack@marqo.ai>
2023-06-20 11:47:17 +1000

    types and docstrings for BaseANN

--- ann_benchmarks/algorithms/base/module.py
+++ ann_benchmarks/algorithms/base/module.py
@@ -1,36 +1,79 @@
 from multiprocessing.pool import ThreadPool
-
+from typing import Any, Dict, Optional
 import psutil
 
+import numpy
 
 class BaseANN(object):
-    def done(self):
+    """Base class/interface for Approximate Nearest Neighbors (ANN) algorithms used in benchmarking."""
+
+    def done(self) -> None:
+        """Clean up BaseANN once it is finished being used."""
         pass
 
-    def get_memory_usage(self):
-        """Return the current memory usage of this algorithm instance
-        (in kilobytes), or None if this information is not available."""
-        # return in kB for backwards compatibility
+    def get_memory_usage(self) -> Optional[float]:
+        """Returns the current memory usage of this ANN algorithm instance in kilobytes.
+
+        Returns:
+            float: The current memory usage in kilobytes (for backwards compatibility), or None if
+                this information is not available.
+        """
+
         return psutil.Process().memory_info().rss / 1024
 
-    def fit(self, X):
+    def fit(self, X: numpy.array) -> None:
+        """Fits the ANN algorithm to the provided data. 
+
+        Note: This is a placeholder method to be implemented by subclasses.
+
+        Args:
+            X (numpy.array): The data to fit the algorithm to.
+        """
         pass
 
-    def query(self, q, n):
+    def query(self, q: numpy.array, n: int) -> numpy.array:
+        """Performs a query on the algorithm to find the nearest neighbors. 
+
+        Note: This is a placeholder method to be implemented by subclasses.
+
+        Args:
+            q (numpy.array): The vector to find the nearest neighbors of.
+            n (int): The number of nearest neighbors to return.
+
+        Returns:
+            numpy.array: An array of indices representing the nearest neighbors.
+        """
         return []  # array of candidate indices
 
-    def batch_query(self, X, n):
-        """Provide all queries at once and let algorithm figure out
-        how to handle it. Default implementation uses a ThreadPool
-        to parallelize query processing."""
+    def batch_query(self, X: numpy.array, n: int) -> None:
+        """Performs multiple queries at once and lets the algorithm figure out how to handle it.
+
+        The default implementation uses a ThreadPool to parallelize query processing.
+
+        Args:
+            X (numpy.array): An array of vectors to find the nearest neighbors of.
+            n (int): The number of nearest neighbors to return for each query.
+        Returns: 
+            None: self.get_batch_results() is responsible for retrieving batch result
+        """
         pool = ThreadPool()
         self.res = pool.map(lambda q: self.query(q, n), X)
 
-    def get_batch_results(self):
+    def get_batch_results(self) -> numpy.array:
+        """Retrieves the results of a batch query (from .batch_query()).
+
+        Returns:
+            numpy.array: An array of nearest neighbor results for each query in the batch.
+        """
         return self.res
 
-    def get_additional(self):
+    def get_additional(self) -> Dict[str, Any]:
+        """Returns additional attributes to be stored with the result.
+
+        Returns:
+            dict: A dictionary of additional attributes.
+        """
         return {}
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
