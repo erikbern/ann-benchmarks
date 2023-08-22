80174dd94702f2554a871282aa642141fc68ef96
Andrew Kane <andrew@ankane.org>
2023-08-22 01:17:11 -0700

    Fix dbpedia-openai-*k-angular datasets

--- ann_benchmarks/datasets.py
+++ ann_benchmarks/datasets.py
@@ -601,6 +601,6 @@ DATASETS: Dict[str, Callable[[str], None]] = {
 }
 
 DATASETS.update({
-    f"dbpedia-openai-{n//1000}k-angular": lambda out_fn: dbpedia_entities_openai_1M(out_fn, n)
+    f"dbpedia-openai-{n//1000}k-angular": lambda out_fn, i=n: dbpedia_entities_openai_1M(out_fn, i)
     for n in range(100_000, 1_100_000, 100_000)
 })
