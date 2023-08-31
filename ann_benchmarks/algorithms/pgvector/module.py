17969aef193062e6a5247765447e6397dde81142
Andrew Kane <andrew@ankane.org>
2023-08-31 15:35:47 -0700

    Use HNSW for pgvector

--- ann_benchmarks/algorithms/pgvector/module.py
+++ ann_benchmarks/algorithms/pgvector/module.py
@@ -8,9 +8,10 @@ from ..base.module import BaseANN
 
 
 class PGVector(BaseANN):
-    def __init__(self, metric, lists):
+    def __init__(self, metric, method_param):
         self._metric = metric
-        self._lists = lists
+        self._m = method_param['M']
+        self._ef_construction = method_param['efConstruction']
         self._cur = None
 
         if metric == "angular":
@@ -22,9 +23,10 @@ class PGVector(BaseANN):
 
     def fit(self, X):
         subprocess.run("service postgresql start", shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
-        conn = psycopg.connect(user="ann", password="ann", dbname="ann")
+        conn = psycopg.connect(user="ann", password="ann", dbname="ann", autocommit=True)
         pgvector.psycopg.register_vector(conn)
         cur = conn.cursor()
+        cur.execute("DROP TABLE IF EXISTS items")
         cur.execute("CREATE TABLE items (id int, embedding vector(%d))" % X.shape[1])
         cur.execute("ALTER TABLE items ALTER COLUMN embedding SET STORAGE PLAIN")
         print("copying data...")
@@ -34,22 +36,18 @@ class PGVector(BaseANN):
         print("creating index...")
         if self._metric == "angular":
             cur.execute(
-                "CREATE INDEX ON items USING ivfflat (embedding vector_cosine_ops) WITH (lists = %d)" % self._lists
+                "CREATE INDEX ON items USING hnsw (embedding vector_cosine_ops) WITH (m = %d, ef_construction = %d)" % (self._m, self._ef_construction)
             )
         elif self._metric == "euclidean":
-            cur.execute("CREATE INDEX ON items USING ivfflat (embedding vector_l2_ops) WITH (lists = %d)" % self._lists)
+            cur.execute("CREATE INDEX ON items USING hnsw (embedding vector_l2_ops) WITH (m = %d, ef_construction = %d)" % (self._m, self._ef_construction))
         else:
             raise RuntimeError(f"unknown metric {self._metric}")
         print("done!")
         self._cur = cur
 
-    def set_query_arguments(self, probes):
-        self._probes = probes
-        self._cur.execute("SET ivfflat.probes = %d" % probes)
-        # TODO set based on available memory
-        self._cur.execute("SET work_mem = '256MB'")
-        # disable parallel query execution
-        self._cur.execute("SET max_parallel_workers_per_gather = 0")
+    def set_query_arguments(self, ef_search):
+        self._ef_search = ef_search
+        self._cur.execute("SET hnsw.ef_search = %d" % ef_search)
 
     def query(self, v, n):
         self._cur.execute(self._query, (v, n), binary=True, prepare=True)
@@ -62,4 +60,4 @@ class PGVector(BaseANN):
         return self._cur.fetchone()[0] / 1024
 
     def __str__(self):
-        return f"PGVector(lists={self._lists}, probes={self._probes})"
+        return f"PGVector(m={self._m}, ef_construction={self._ef_construction}, ef_search={self._ef_search})"
