efed3538aa4ae7897ebae599cf512e56f517c07b
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2023-11-09 15:20:49 +0000

    Use http/rest

--- ann_benchmarks/algorithms/surreal_mtree/module.py
+++ ann_benchmarks/algorithms/surreal_mtree/module.py
@@ -1,9 +1,8 @@
 import subprocess
 import sys
-import json
+import requests
 
 from ..base.module import BaseANN
-from surrealdb import SurrealDB
 from time import sleep
 
 class SurrealMtree(BaseANN):        
@@ -15,37 +14,65 @@ class SurrealMtree(BaseANN):
         subprocess.run(f"surreal start --allow-all -u ann -p ann -b 127.0.0.1:8000 {path}  &", shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
         print("wait for the server to be up...")
         sleep(5)
-        self._connection = SurrealDB("ws://127.0.0.1:8000/ann/ann")
-        self._connection.signin({
-            "username": "ann",
-            "password": "ann",
-        })  
+        self._session = requests.Session()
+        self._session.auth = ('ann', 'ann')
+        headers={
+            "NS": 'ann',
+            "DB": 'ann',
+            "Accept": "application/json",
+        }
+        self._session.headers.update(headers)
 
-    def _query(self, q):
-        r = self._connection.query(q)
-        if r is None:
-            raise RuntimeError(f"None response: {q}")
-        if len(r) != 1:
-            raise RuntimeError(f"Incorrect response: {q} => {r}")
+    def _sql(self, q):
+        r = self._session.post(f'http://127.0.0.1:8000/sql', q)
+        if r.status_code != 200:
+            raise RuntimeError(f"{r}")
         return r
 
     def fit(self, X):
         print("copying data...")
-        for i, embedding in enumerate(X):
-            self._query(f"CREATE items:{i} SET i={i}, r={embedding.tolist()};")
+
         dim = X.shape[1]
-        print("creating index...")
+
+        # Fit the database per batch
+        batch = max(10000 // dim, 1)
+        q = ""
+        l = 0
+        t = 0
+        for i, embedding in enumerate(X):
+            l += 1
+            q += f"CREATE items:{i} SET i={i}, r={embedding.tolist()};"
+            if l == batch:
+                self._sql(q)
+                q = ''
+                t += l
+                l = 0
+        if l > 0:
+            self._sql(q)
+            t += l
+        print(f"{t} vectors ingested")
+
+        print(f"creating index - capacity: {self._capacity} - dim: {dim}")
         if self._metric == "euclidean":
-            self._query(f"DEFINE INDEX ix ON items FIELDS r MTREE DIMENSION {dim} DIST EUCLIDEAN TYPE F64 CAPACITY {self._capacity};")
+            self._sql(f"DEFINE INDEX ix ON items FIELDS r MTREE DIMENSION {dim} DIST EUCLIDEAN TYPE F64 CAPACITY {self._capacity};")
         else:
             raise RuntimeError(f"unknown metric {self.metric}")
         print("Index construction done")     
 
     def query(self, v, n):
-        # q = f"SELECT i FROM items WHERE r <{n}> {v.tolist()};"
-        self._query('SELECT count() FROM items;')
-        res = self._connection.query(q)
-        return [item['i'] for item in res[0]['result']]
+        r = self._sql(f"SELECT i FROM items WHERE r <{n}> {v.tolist()};")
+        j = r.json()
+        c = 0
+        items = []
+        for item in j[0]['result']:
+            c += 1
+            items.append(item['i'])
+        if c != n:
+            raise RuntimeError(f"Invalid items count: {c} => {j}")
+        return items
 
     def __str__(self):
         return f"SurrealMtree(path={self._path}, capacity={self._capacity})"
+
+    def close(self):
+        self._session.close()
