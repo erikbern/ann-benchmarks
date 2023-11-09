1021bc49367b9e7a3625d060f7355bd621430233
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2023-11-09 13:01:39 +0000

    Wip on sync client

--- ann_benchmarks/algorithms/surreal_mtree/module.py
+++ ann_benchmarks/algorithms/surreal_mtree/module.py
@@ -1,58 +1,51 @@
 import subprocess
 import sys
-import asyncio
-import threading
+import json
 
 from ..base.module import BaseANN
-from surrealdb import Surreal
-from concurrent.futures import ThreadPoolExecutor
+from surrealdb import SurrealDB
 from time import sleep
-from functools import partial
 
 class SurrealMtree(BaseANN):        
 
     def __init__(self, metric, path = 'memory', capacity = 40):
-        self.loop = asyncio.get_event_loop()
-        self.metric = metric
-        self.path = path
-        self.capacity = capacity
+        self._metric = metric
+        self._path = path
+        self._capacity = capacity
         subprocess.run(f"surreal start --allow-all -u ann -p ann -b 127.0.0.1:8000 {path}  &", shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
         print("wait for the server to be up...")
         sleep(5)
+        self._connection = SurrealDB("ws://127.0.0.1:8000/ann/ann")
+        self._connection.signin({
+            "username": "ann",
+            "password": "ann",
+        })  
 
-    async def async_connect(self, db):
-        await db.connect()
-        await db.signin({"user": "ann", "pass": "ann"})
-        await db.use("ann", "ann")
-
-    async def async_fit(self, X):
-        async with Surreal("ws://localhost:8000/rpc") as db:
-            await self.async_connect(db)
-            print("copying data...")
-            for i, embedding in enumerate(X):
-                await db.create(
-                    f"items:{i}",
-                    { "i": i, "r": embedding.tolist()},
-                )
-            dim = X.shape[1];
-            print("creating index...")
-            if self.metric == "euclidean":
-                await db.query(f"DEFINE INDEX ix ON items FIELDS r MTREE DIMENSION {dim} DIST EUCLIDEAN TYPE F64 CAPACITY {self.capacity}")
-            else:
-                raise RuntimeError(f"unknown metric {self.metric}")
-            print("Index construction done")
+    def _query(self, q):
+        r = self._connection.query(q)
+        if r is None:
+            raise RuntimeError(f"None response: {q}")
+        if len(r) != 1:
+            raise RuntimeError(f"Incorrect response: {q} => {r}")
+        return r
 
     def fit(self, X):
-        self.loop.run_until_complete(self.async_fit(X))
-
-    async def async_query(self, v, n):
-         async with Surreal("ws://localhost:8000/rpc") as db:
-            await self.async_connect(db)
-            res = await db.query(f"SELECT i FROM items WHERE r <{n}> {v.tolist()}")
-            return [item['i'] for item in res[0]['result']]
+        print("copying data...")
+        for i, embedding in enumerate(X):
+            self._query(f"CREATE items:{i} SET i={i}, r={embedding.tolist()};")
+        dim = X.shape[1]
+        print("creating index...")
+        if self._metric == "euclidean":
+            self._query(f"DEFINE INDEX ix ON items FIELDS r MTREE DIMENSION {dim} DIST EUCLIDEAN TYPE F64 CAPACITY {self._capacity};")
+        else:
+            raise RuntimeError(f"unknown metric {self.metric}")
+        print("Index construction done")     
 
     def query(self, v, n):
-        return self.loop.run_until_complete(self.async_query(v, n))
+        # q = f"SELECT i FROM items WHERE r <{n}> {v.tolist()};"
+        self._query('SELECT count() FROM items;')
+        res = self._connection.query(q)
+        return [item['i'] for item in res[0]['result']]
 
     def __str__(self):
-        return f"SurrealMtree(path={self.path}, capacity={self.capacity})"
+        return f"SurrealMtree(path={self._path}, capacity={self._capacity})"
