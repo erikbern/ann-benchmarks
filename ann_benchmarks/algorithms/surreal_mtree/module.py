63940f71f3007f960198078c8669aab7d4c5a26d
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2023-11-09 01:02:08 +0000

    Surreal bench run

--- ann_benchmarks/algorithms/surreal_mtree/module.py
+++ ann_benchmarks/algorithms/surreal_mtree/module.py
@@ -1,68 +1,72 @@
 import subprocess
 import sys
 import asyncio
+import threading
 
 from ..base.module import BaseANN
 from surrealdb import Surreal
 from concurrent.futures import ThreadPoolExecutor
 from time import sleep
+from functools import partial
 
-class SurrealMtree(BaseANN):
-    def __init__(self, metric, method_param):
+class SurrealMtree(BaseANN):        
+
+    def __init__(self, metric, capacity = 40):
+        self.loop = asyncio.new_event_loop()
         self.executor = ThreadPoolExecutor()
+        self.thread = threading.Thread(target=self.start_loop, args=(self.loop,), daemon=True)
+        self.thread.start()
         self.metric = metric
-        self.ef = method_param['efConstruction']
-        self.m = method_param["M"]
-        self.db = None
-
-        if metric == "angular":
-            self._query = "SELECT id FROM items ORDER BY embedding <=> %s::real[] LIMIT %s"
-        elif metric == "euclidean":
-            self._query = "SELECT id FROM items ORDER BY embedding <-> %s::real[] LIMIT %s"
-        else:
-            raise RuntimeError(f"unknown metric {metric}")
-
-    async def async_fit(self, X):
+        self.capacity = capacity
         subprocess.run("surreal start --allow-all -u ann -p ann -b 127.0.0.1:8000 memory  &", shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
         print("wait for the server to be up...")
-        sleep(5);
-        self.db = Surreal("ws://127.0.0.1:8000/rpc")
-        await self.db.connect()
-        await self.db.signin({"user": "ann", "pass": "ann"})
-        await self.db.use("ann", "ann")
-        print("copying data...")
-        for i, embedding in enumerate(X):
-            await self.db.create(
-                f"items:{id}",
-                { "real": embedding.tolist()},
-            )
-        print("creating index...%d", X.shape[1])
-        if self.metric == "euclidean":
-            print(await self.db.query("DEFINE INDEX ix ON items FIELDS real MTREE DIMENSION %d DIST EUCLIDEAN TYPE F64" % X.shape[1]))
-        else:
-            raise RuntimeError(f"unknown metric {self.metric}")
-        print("Index construction done!")
+        sleep(5)
+
+    def start_loop(self, loop):
+        asyncio.set_event_loop(loop)
+        loop.run_forever()
+
+    async def async_connect(self, db):
+        await db.connect()
+        await db.signin({"user": "ann", "pass": "ann"})
+        await db.use("ann", "ann")
+
+    async def async_fit(self, X):
+        async with Surreal("http://127.0.0.1:8000/rpc") as db:
+            await self.async_connect(db)
+            print("copying data...")
+            for i, embedding in enumerate(X):
+                await db.create(
+                    f"items:{i}",
+                    { "i": i, "r": embedding.tolist()},
+                )
+            dim = X.shape[1];
+            print("creating index...")
+            if self.metric == "euclidean":
+                await db.query(f"DEFINE INDEX ix ON items FIELDS r MTREE DIMENSION {dim} DIST EUCLIDEAN TYPE F64 CAPACITY {self.capacity}")
+            else:
+                raise RuntimeError(f"unknown metric {self.metric}")
+            print("Index construction done")
 
     def fit(self, X):
-        future = self.executor.submit(asyncio.run, self.async_fit(X))
+        coro = self.async_fit(X)
+        future = asyncio.run_coroutine_threadsafe(coro, self.loop)
         future.result()
 
-    def set_query_arguments(self, ef):
-        self.ef = ef
-
     async def async_query(self, v, n):
-        q = f"SELECT id FROM items WHERE real <{n}> {v.tolist()}"
-        print(q)
-        r = await self.db.query(q)
-        print(r)
-        return [id for id, in self._cur.fetchall()]
+         async with Surreal("http://127.0.0.1:8000/rpc") as db:
+            await self.async_connect(db)
+            res = await db.query(f"SELECT i FROM items WHERE r <{n}> {v.tolist()}")
+            return [item['i'] for item in res[0]['result']]
 
     def query(self, v, n):
-        future = self.executor.submit(asyncio.run, self.async_query(v, n))
+        coro = self.async_query(v, n)
+        future = asyncio.run_coroutine_threadsafe(coro, self.loop)
         return future.result()
 
     def __str__(self):
-        return f"SurrealMtree(m={self.m}, ef={self.ef})"
+        return f"SurrealMtree(capacity={self.capacity})"
 
     def close(self):
-        self.executor.shutdown(wait=True)
+        self.loop.call_soon_threadsafe(self.loop.stop)
+        self.thread.join()
