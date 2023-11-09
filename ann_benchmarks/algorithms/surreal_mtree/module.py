3fc279db3204e6cdd9f58f7b97af163d72a37752
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2023-11-09 10:56:47 +0000

    Add path init parameter

--- ann_benchmarks/algorithms/surreal_mtree/module.py
+++ ann_benchmarks/algorithms/surreal_mtree/module.py
@@ -11,14 +11,15 @@ from functools import partial
 
 class SurrealMtree(BaseANN):        
 
-    def __init__(self, metric, capacity = 40):
+    def __init__(self, metric, path = 'memory', capacity = 40):
         self.loop = asyncio.new_event_loop()
         self.executor = ThreadPoolExecutor()
         self.thread = threading.Thread(target=self.start_loop, args=(self.loop,), daemon=True)
         self.thread.start()
         self.metric = metric
+        self.path = path
         self.capacity = capacity
-        subprocess.run("surreal start --allow-all -u ann -p ann -b 127.0.0.1:8000 memory  &", shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
+        subprocess.run(f"surreal start --allow-all -u ann -p ann -b 127.0.0.1:8000 {path}  &", shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
         print("wait for the server to be up...")
         sleep(5)
 
@@ -32,7 +33,7 @@ class SurrealMtree(BaseANN):
         await db.use("ann", "ann")
 
     async def async_fit(self, X):
-        async with Surreal("http://127.0.0.1:8000/rpc") as db:
+        async with Surreal("ws://localhost:8000/rpc") as db:
             await self.async_connect(db)
             print("copying data...")
             for i, embedding in enumerate(X):
@@ -54,7 +55,7 @@ class SurrealMtree(BaseANN):
         future.result()
 
     async def async_query(self, v, n):
-         async with Surreal("http://127.0.0.1:8000/rpc") as db:
+         async with Surreal("ws://localhost:8000/rpc") as db:
             await self.async_connect(db)
             res = await db.query(f"SELECT i FROM items WHERE r <{n}> {v.tolist()}")
             return [item['i'] for item in res[0]['result']]
@@ -65,7 +66,7 @@ class SurrealMtree(BaseANN):
         return future.result()
 
     def __str__(self):
-        return f"SurrealMtree(capacity={self.capacity})"
+        return f"SurrealMtree(path={self.path}, capacity={self.capacity})"
 
     def close(self):
         self.loop.call_soon_threadsafe(self.loop.stop)
