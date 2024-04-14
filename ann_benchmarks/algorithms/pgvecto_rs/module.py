82ef7793da1718fa27f5787c1a0b26c8ade46da4
Keming <kemingy94@gmail.com>
2024-04-14 08:02:33 +0800

    fix index with options
    Signed-off-by: Keming <kemingy94@gmail.com>

--- ann_benchmarks/algorithms/pgvecto_rs/module.py
+++ ann_benchmarks/algorithms/pgvecto_rs/module.py
@@ -53,10 +53,10 @@ class PGVectoRS(BaseANN):
 
         if metric == "angular":
             self.query_sql = "SELECT id FROM items ORDER BY embedding <=> %s LIMIT %s"
-            self.index_sql = "CREATE INDEX ON items USING vectors (embedding vector_cos_ops)"
+            self.index_sql = f"CREATE INDEX ON items USING vectors (embedding vector_cos_ops) WITH (options = $$[indexing.hnsw]\nm = {self.m}\nef_construction = {self.ef_construction}$$)"
         elif metric == "euclidean":
             self.query_sql = "SELECT id FROM items ORDER BY embedding <-> %s LIMIT %s"
-            self.index_sql = "CREATE INDEX ON items USING vectors (embedding vector_l2_ops)"
+            self.index_sql = "CREATE INDEX ON items USING vectors (embedding vector_l2_ops) WITH (options = $$[indexing.hnsw]\nm = {self.m}\nef_construction = {self.ef_construction}$$)"
         else:
             raise RuntimeError(f"unknown metric {metric}")
 
