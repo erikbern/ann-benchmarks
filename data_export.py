3982de0c267c9e3de65044b5c55f143917df7023
Yinzuo Jiang <jiangyinzuo@foxmail.com>
2024-05-17 15:45:03 +0800

    add `--count` and `--batch` args for data_export.py

--- data_export.py
+++ data_export.py
@@ -9,14 +9,21 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--output", help="Path to the output file", required=True)
     parser.add_argument("--recompute", action="store_true", help="Recompute metrics")
+    parser.add_argument(
+        "-k", "--count", default=10, type=int, help="The number of near neighbours to search for"
+    )
+    parser.add_argument("--batch", action="store_true", help="Batch mode")
     args = parser.parse_args()
 
     datasets = DATASETS.keys()
     dfs = []
     for dataset_name in datasets:
         print("Looking at dataset", dataset_name)
-        if len(list(load_all_results(dataset_name))) > 0:
-            results = load_all_results(dataset_name)
+        if len(list(load_all_results(dataset_name,
+                                        count=args.count,
+                                        batch_mode=args.batch
+                                     ))) > 0:
+            results = load_all_results(dataset_name, count=args.count, batch_mode=args.batch)
             dataset, _ = get_dataset(dataset_name)
             results = compute_metrics_all_runs(dataset, results, args.recompute)
             for res in results:
